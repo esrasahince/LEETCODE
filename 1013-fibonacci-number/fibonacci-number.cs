eb3e5e128c599eaa2ca50d0bd3d6fbce28bec4c4
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2024-04-15 10:43:06 +0300

    Time: 17 ms (88.47%) | Memory: 27.5 MB (5.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -1,11 +1,22 @@
 public class Solution {
     public int Fib(int n) {
 
-        if(n==0 || n==1)
-      
-        return n;
-        
-        return Fib(n-1)+Fib(n-2);
+       
+       Dictionary<int,int> memo=new();
+       memo[0]=0;
+       memo[1]=1;
+       return DynamicProgramming(n,memo);
+
+    }
+    public int DynamicProgramming(int n, Dictionary<int,int> memo)
+    {
+        if(memo.Keys.Contains(n))
+        return memo[n];
+        else{
+             int result=DynamicProgramming(n-1,memo)+DynamicProgramming(n-2,memo);
+             memo[n]=result;
+             return result;
+        }
     }
 
    
