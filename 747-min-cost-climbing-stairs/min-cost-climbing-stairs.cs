ec6b003b7ddf5abbfdfed78a7481f4cffca4dbf2
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2024-04-16 07:55:44 +0300

    Time: 70 ms (57.94%) | Memory: 42.1 MB (57.47%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
-        if(cost.Length<2)
-        return cost[0];
+     
         for(int i=cost.Length-3;i>=0;i--)
         {
             cost[i]=Math.Min(cost[i+1],cost[i+2])+cost[i];
