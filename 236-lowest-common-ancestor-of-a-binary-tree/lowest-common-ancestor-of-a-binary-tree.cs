81c3aec6d152aa1d9220122d99a2dbfda581f75c
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2024-05-28 19:48:20 +0300

    Time: 88 ms (41.31%) | Memory: 46 MB (80.66%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cs
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cs
@@ -9,41 +9,23 @@
  */
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> pathToP = new List<TreeNode>();
-        List<TreeNode> pathToQ = new List<TreeNode>();
-
-        BackTrack(root, p, pathToP);
-        BackTrack(root, q, pathToQ);
-
-        Console.WriteLine("Path to p:");
-        foreach (var node in pathToP) {
-            Console.Write(node.val + " ");
-        }
-        Console.WriteLine();
         
-        Console.WriteLine("Path to q:");
-        foreach (var node in pathToQ) {
-            Console.Write(node.val + " ");
-        }
-        Console.WriteLine();
-
-        var commonPath = pathToP.Intersect(pathToQ).ToList();
-        return commonPath[commonPath.Count-1];
-    }
+       if(root==null||root==p||root==q)
+       return root;
     
-    public bool BackTrack(TreeNode root, TreeNode node, List<TreeNode> path) {
-        if (root == null)
-            return false;
 
-        path.Add(root);
+        var left= LowestCommonAncestor(root.left,p,q);
+        var right= LowestCommonAncestor(root.right,p,q);
 
-        if (root.val == node.val)
-            return true;
+        if(left!=null&&right!=null)
+        return root;
+        else 
+        return left!=null?left:right;
+        
 
-        if (BackTrack(root.left, node, path) || BackTrack(root.right, node, path))
-            return true;
+        
+        
+       
 
-        path.RemoveAt(path.Count - 1);
-        return false;
     }
-    }
+}
