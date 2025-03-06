b4c67ceadef616eb6d2e88049055f5f2e1d0a0fd
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-03-06 09:56:33 +0300

    Time: 33 ms (24.41%) | Memory: 44.3 MB (17.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,28 +1,18 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        int slen=s.Length;
-        int tlen=t.Length;
-        if(slen!=tlen)
-        return false;
-        List<int> s1=new();
-        List<int> t1=new();
 
-       
+        Char[] s1=s.ToCharArray();
+        Char[] t1=t.ToCharArray();
+        Array.Sort(s1);
+        Array.Sort(t1);
 
+        
+       String snew= new String(s1);
+       String tnew= new String(t1);
 
-        for(int i=0;i<slen;i++)
-        {
-            s1.Add((int)s[i]);
-            t1.Add((int)t[i]);
-
-        }
-        s1.Sort();
-        t1.Sort();
-        if(s1.SequenceEqual(t1))
+        if(snew==tnew)
         return true;
         return false;
-
-
         
     }
 }
