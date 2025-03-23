8cafef6748e9422566c604a3b5c6957671bc2e57
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-03-23 06:38:54 +0300

    Time: 415 ms (16.15%) | Memory: 175.6 MB (23.44%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cs
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cs
@@ -1,40 +1,58 @@
+
 public class Solution {
     public bool ValidPath(int n, int[][] edges, int source, int destination) {
- if(n==1)
- return true;
-        Dictionary<int,List<int>> graph=new();
-        Dictionary<int,bool> visited=new();
+
+        //I need to create a dictionary like.  vertex number and its neighbours
+        //I need a visited list
+        //for exp
+        //0: 4,9
+        //1: 4,7
+        //2: 4
+        //3: 4
+        //4: 3,1,8,6,2,7,0,5
+        //5: 4
+        //6: 4,
+        //7:1,4
+        //8:4,
+        //9:0
+        // for loop 
+        //ınvestigate all child whether having dest or not
+        if(edges.Length==0||edges==null||source==destination)
+        return true;
+        
+        Dictionary<int,HashSet<int>> edge=new();
         for(int i=0;i<n;i++)
         {
-            graph[i]=new List<int>();
-            visited[i]=false;
+                edge[i]=new HashSet<int>();
         }
-        foreach(var item in edges)
+        
+        for(int i=0;i<edges.Length;i++)
         {
-            graph[item[0]].Add(item[1]);
-            graph[item[1]].Add(item[0]);
+            edge[edges[i][0]].Add(edges[i][1]);
+            edge[edges[i][1]].Add(edges[i][0]);
         }
-
-        //BFS
-
-       Queue<int> queue=new();
-       queue.Enqueue(source);
-       visited[source]=true;
-       while(queue.Count>0)
-       {
-        int node=queue.Dequeue();
-        foreach(int child in graph[node])
-        {
-            if(child==destination)
-            return true;
-            if(visited[child]==false)
-            {
-            queue.Enqueue(child);
-            visited[child]=true;
-            }
-        }
-       }
-       return false;
-
+        HashSet<int> visited=new();
+        return Check(edge,source,destination,visited);
+        
+        
     }
+    public bool Check(Dictionary<int,HashSet<int>> edge, int source,int dest, HashSet<int> visited )
+   {
+    if(edge[source].Contains(dest))
+    return true;
+    visited.Add(source);
+    foreach(int value in edge[source])
+    {
+        
+                if(!visited.Contains(value))
+                {
+                
+                     if (Check(edge, value, dest, visited))
+                    return true;
+                }       
+            
+    }
+    return false;
+
+   } 
 }
