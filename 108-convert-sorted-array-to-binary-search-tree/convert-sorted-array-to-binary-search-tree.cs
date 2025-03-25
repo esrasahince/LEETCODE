b3d6c5827ca564c2a8c793fb2123b9d91523f466
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-03-25 09:39:04 +0300

    Time: 0 ms (100.00%) | Memory: 44.9 MB (50.20%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cs
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cs
@@ -13,26 +13,35 @@
  */
 public class Solution {
     public TreeNode SortedArrayToBST(int[] nums) {
-        if (nums == null || nums.Length == 0) {
-            return null;
-        }
 
-        TreeNode root=Helper(0,nums.Length-1,nums);
-    return root;
+        if(nums==null||nums.Length==0)
+            return new TreeNode();
+        if(nums.Length==1)
+            return new TreeNode(nums[0]);
+        int left=0;
+        int right=nums.Length-1;
+    
+        return Insert(nums,left,right);
 
         
-    }
+}
 
- 
-    public TreeNode Helper(int left, int right, int[] nums)
-    {
+    public TreeNode Insert(int[] nums,int left, int right) {
+        
         if(left>right)
         return null;
-        int middle=left+(right-left)/2;
-        TreeNode root=new TreeNode(nums[middle]);
-        root.left=Helper(left,middle-1,nums);
-        root.right=Helper(middle+1,right,nums);
-        return root;
 
+        int middle=(right-left)/2+left;
+        TreeNode root=new TreeNode(nums[middle]);
+        root.left=Insert(nums,left,middle-1);
+        root.right=Insert(nums,middle+1,right);
+        return root;
+    
+       
+
+
+   
     }
+
+
 }
