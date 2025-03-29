458c074873df6aa4f95f2268c62ed1d2e2542413
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-03-29 13:49:46 +0300

    Time: 5 ms (28.63%) | Memory: 85.4 MB (17.15%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        if(root==null)
+        return 0;
+        int result=int.MaxValue;
+         DFS(root,1,ref result);
+         return result;
+
+        
+    }
+    public int DFS(TreeNode current,int level, ref int result)
+    {
+        if(current==null)
+        return level;
+        if(current.left==null&&current.right==null)
+        result=Math.Min(level,result);
+        int left=DFS(current.left,level+1,ref result);
+        int right=DFS(current.right,level+1,ref result);
+        return Math.Max(left,right);
+    }
+}
