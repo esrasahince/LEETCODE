32d55e0c3c12483bc97f60e7f5597d67db6fd1f5
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-03-29 13:01:28 +0300

    Time: 0 ms (100.00%) | Memory: 43.4 MB (73.17%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        return DFS(root);
+        
+    }
+    public int DFS(TreeNode root)
+    {
+        if(root==null)
+        { 
+            return 0;
+        }
+        return Math.Max(DFS(root.left),DFS(root.right))+1;
+    }
+}
