102fb3fe29b307d618b2505487e657ba9268d789
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-01 09:47:21 +0300

    Time: 4 ms (40.38%) | Memory: 47.6 MB (33.85%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cs
+++ 871-keys-and-rooms/keys-and-rooms.cs
@@ -9,17 +9,19 @@ public class Solution {
           HashSet<int> visited=new();
           Queue<int> que=new();
           que.Enqueue(0);
+          visited.Add(0);
           while(que.Count>0)
           {
             int visit=que.Dequeue();
             if(visit>=rooms.Count||visit<0)
             continue;
-            visited.Add(visit);
+           
           
             foreach(var item in rooms[visit])
             {
                 if(!visited.Contains(item))
                 que.Enqueue(item);
+                 visited.Add(item);
             }
           }
           if(visited.Count==rooms.Count)
