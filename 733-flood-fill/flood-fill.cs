43ddfcf2ed50fb841a5196a085933b7038c618a8
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-01 08:58:16 +0300

    Time: 1 ms (45.59%) | Memory: 50.1 MB (48.66%) - LeetSync

--- 733-flood-fill/flood-fill.cs
+++ 733-flood-fill/flood-fill.cs
@@ -3,7 +3,6 @@ public class Solution {
         if(image==null||image.Length==0||image[sr][sc] == color)
         return image;
 
-
         BFS(image,sr,sc,color);
         return image;
         
@@ -13,13 +12,13 @@ public class Solution {
         Queue<(int,int)> que=new();
         int i=image[nrow][ncolumn];
         que.Enqueue((nrow,ncolumn));
-        HashSet<(int,int)> visited=new();
+    
         while(que.Count>0)
         {
             var (row,column)=que.Dequeue();
-            if(row<0||row>=image.Length||column<0||column>=image[0].Length||image[row][column]!=i||visited.Contains((row,column)))
+            if(row<0||row>=image.Length||column<0||column>=image[0].Length||image[row][column]!=i)
             continue;
-            visited.Add((row,column));
+          
             image[row][column]=color;
             que.Enqueue((row+1,column));
             que.Enqueue((row-1,column));
