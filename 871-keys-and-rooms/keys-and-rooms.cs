d9ba5ff60b2519dcd414bbfb903f1a0faa593e30
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-01 09:44:09 +0300

    Time: 7 ms (17.31%) | Memory: 47.7 MB (17.69%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool CanVisitAllRooms(IList<IList<int>> rooms) {
+        if(rooms==null||rooms.Count==0)
+        return true;
+        return BFS();
+        
+        bool BFS()
+        {
+          HashSet<int> visited=new();
+          Queue<int> que=new();
+          que.Enqueue(0);
+          while(que.Count>0)
+          {
+            int visit=que.Dequeue();
+            if(visit>=rooms.Count||visit<0)
+            continue;
+            visited.Add(visit);
+          
+            foreach(var item in rooms[visit])
+            {
+                if(!visited.Contains(item))
+                que.Enqueue(item);
+            }
+          }
+          if(visited.Count==rooms.Count)
+          return true;
+          else return false;
+
+        }
+        
+    }
+}
