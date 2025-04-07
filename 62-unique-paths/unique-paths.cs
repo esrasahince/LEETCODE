2e636cf04ac5473a7bb05f026d4c0921cec81cbe
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-07 21:32:52 +0300

    Time: 2 ms (38.60%) | Memory: 28.8 MB (80.02%) - LeetSync

--- 62-unique-paths/unique-paths.cs
+++ 62-unique-paths/unique-paths.cs
@@ -1,18 +1,36 @@
 public class Solution {
     public int UniquePaths(int m, int n) {
-        Dictionary<(int,int),int> memo=new();
-        return DFS(m,n,0,0,memo);
+        return BottomUp(m,n);
+        
     }
-    public int DFS(int row,int column,int crow,int ccolumn,Dictionary<(int,int),int> memo)
+    public int BottomUp(int row,int column)
     {
-        if(crow>=row||ccolumn>=column)
-        return 0;
-        if(crow==row-1&&ccolumn==column-1)
-        return 1;
-        if(memo.ContainsKey((crow,ccolumn)))
-        return memo[(crow,ccolumn)];
-        int result=DFS(row,column,crow+1,ccolumn,memo)+DFS(row,column,crow,ccolumn+1,memo);
-        memo[(crow,ccolumn)]=result;
-        return result;
+        int[,] grid=new int[row,column];
+        grid[row-1,column-1]=1;
+      
+
+        for(int i=row-1;i>=0;i--)
+        {
+            for(int j=column-1;j>=0;j--)
+            {
+                if(i==row-1&&j==column-1)
+                {
+                grid[row-1,column-1]=1;
+                continue;
+                }
+                int right=0;
+                int down=0;
+                if(j+1<=column-1)
+                {
+                  right=grid[i,j+1];
+                }
+                   if(i+1<=row-1)
+                {
+                  down=grid[i+1,j];
+                }
+                grid[i,j]=down+right;
+            }
+        }
+        return grid[0,0];
     }
 }
