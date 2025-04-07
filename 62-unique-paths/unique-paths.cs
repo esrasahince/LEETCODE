372c07eb70f905a61d546579507d7815d955777c
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-07 21:52:28 +0300

    Time: 0 ms (100.00%) | Memory: 29 MB (71.87%) - LeetSync

--- 62-unique-paths/unique-paths.cs
+++ 62-unique-paths/unique-paths.cs
@@ -1,36 +1,17 @@
 public class Solution {
     public int UniquePaths(int m, int n) {
-        return BottomUp(m,n);
-        
-    }
-    public int BottomUp(int row,int column)
-    {
-        int[,] grid=new int[row,column];
-        grid[row-1,column-1]=1;
-      
-
-        for(int i=row-1;i>=0;i--)
+        int[] tempRow=new int[n];
+        for (int i = 0; i < n; i++)
         {
-            for(int j=column-1;j>=0;j--)
+            tempRow[i] = 1;
+        }
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j=tempRow.Length-2;j>=0;j--)
             {
-                if(i==row-1&&j==column-1)
-                {
-                grid[row-1,column-1]=1;
-                continue;
-                }
-                int right=0;
-                int down=0;
-                if(j+1<=column-1)
-                {
-                  right=grid[i,j+1];
-                }
-                   if(i+1<=row-1)
-                {
-                  down=grid[i+1,j];
-                }
-                grid[i,j]=down+right;
+                tempRow[j]=tempRow[j+1]+tempRow[j];
             }
         }
-        return grid[0,0];
+        return tempRow[0];
     }
 }
