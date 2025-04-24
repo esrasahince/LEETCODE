bf046c84720f2ebe877f30408489e2157b7dec01
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-24 08:21:00 +0300

    Time: 100 ms (87.78%) | Memory: 49.4 MB (64.44%) - LeetSync

---
+++ 285-inorder-successor-in-bst/inorder-successor-in-bst.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InorderSuccessor(TreeNode root, TreeNode p) {
+        List<TreeNode> result=new();
+
+         InOrderSearch(root,result);
+         int index=0;
+        foreach(TreeNode item in result)
+        {
+            if(item.val==p.val)
+            {
+                index=result.IndexOf(item);
+
+            }
+        }
+        
+         return index+1<result.Count?result[index+1]:null;
+
+
+        
+    }
+    public void InOrderSearch(TreeNode node,List<TreeNode> result)
+    {
+        if(node==null)
+        return;
+        InOrderSearch(node.left,result);
+        result.Add(node);
+        InOrderSearch(node.right,result);
+        
+
+
+    }
+}
