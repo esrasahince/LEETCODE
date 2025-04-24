5bdf419df17c13e116bf546867d0716b810cc7ec
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-24 08:40:04 +0300

    Time: 110 ms (44.44%) | Memory: 49.5 MB (52.22%) - LeetSync

--- 285-inorder-successor-in-bst/inorder-successor-in-bst.cs
+++ 285-inorder-successor-in-bst/inorder-successor-in-bst.cs
@@ -9,34 +9,39 @@
  */
 public class Solution {
     public TreeNode InorderSuccessor(TreeNode root, TreeNode p) {
-        List<TreeNode> result=new();
-
-         InOrderSearch(root,result);
-         int index=0;
-        foreach(TreeNode item in result)
-        {
-            if(item.val==p.val)
-            {
-                index=result.IndexOf(item);
-                break;
-
-            }
-        }
-        
-         return index+1<result.Count?result[index+1]:null;
-
-
+        bool result=false;
+	
+	return InorderSuccessor(root,p,ref result);
+	
         
     }
-    public void InOrderSearch(TreeNode node,List<TreeNode> result)
-    {
-        if(node==null)
-        return;
-        InOrderSearch(node.left,result);
-        result.Add(node);
-        InOrderSearch(node.right,result);
-        
+	public TreeNode InorderSuccessor(TreeNode node, TreeNode p, ref bool result)
+	{
+	if(node==null)
+	return null;
 
+	
+	TreeNode leftresult=InorderSuccessor(node.left,p,ref result);
+	if(leftresult!=null)
+	{
+	return leftresult;
+	}
 
-    }
+    if(result)
+	return node;
+	if(node.val==p.val)
+	result=true;
+	TreeNode rightresult=InorderSuccessor(node.right,p,ref result);
+	if(rightresult!=null)
+	return rightresult;
+	
+	return null;
+	
+	
+	
+	
+	
+	
+	
+	}
 }
