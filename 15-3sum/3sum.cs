36b91eace7ba205b93c82d9abf895e381645d401
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-26 09:45:37 +0300

    Time: 33 ms (84.59%) | Memory: 65.7 MB (61.44%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,52 +1,59 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
         List<IList<int>> result=new();
-
-        //sort the array to build logic
+        int len=nums.Length;
+        if(len<3)
+        return result;
         Array.Sort(nums);
-
-        //for each item in nums, use the 2 pointer technique
-        //we want unique items in rresult. so if first pointer, or right or left pointer is same with previous pointer
-        //move the pointer
-
-        //last 2 item for left and right pointer. so nums.Length-2
-        int goal=0;
         int left=0;
-        int right=0;
-        for(int i=0;i<nums.Length-2;i++)
+        int right=len-1;
+        int target=0;
+
+        for(int i=0;i<nums.Length-2;i++) //current value and 2 pointer 
         {
+            //for make unique the result for i pointer;
             if(i>0&&nums[i]==nums[i-1])
-            continue; // for i, check dublicate value
-            goal=-nums[i];
-            //start 2 pointer technique
+            continue;
+            target=-nums[i];
             left=i+1;
-            right=nums.Length-1;
-            
-            while(left<right)
-            {
-                
+            right=len-1;
              
-                int sum=nums[left]+nums[right];
-                if(sum==goal)
+            while(left<right)
+            { //now 2 pointer
+
+                int tempsum=nums[left]+nums[right];
+                if(tempsum==target)
                 {
-                    result.Add(new List<int>(){nums[i],nums[left],nums[right]});
-                        while(left<right&&nums[left]==nums[left+1]){left++;}
-                        while(left<right&&nums[right]==nums[right-1]){right--;}
-                    left++;
-                    right--;
-                }
-                else if(sum<goal)
+                result.Add(new List<int>{nums[i],nums[left],nums[right]});
+                while(left+1<right)
                 {
-                    left++;
+                    if(nums[left]==nums[left+1])
+                    {
+                        left++;
+                    }
+                    else break;
                 }
-                else
-                {right--;}
-            
-            
+                while(right-1>left)
+                {
+                    if(nums[right]==nums[right-1])
+                    {
+                        right--;
+                    }
+                    else break;
+                }
+                left++;
+                right--;
+
+                }
+                else if(tempsum>target)
+                right--;
+                else 
+                left++;
             }
+            
 
         }
         return result;
-    }  
-    
+        
+    }
 }
