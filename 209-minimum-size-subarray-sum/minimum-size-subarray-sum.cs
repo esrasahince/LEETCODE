7b8e9ec48a164b24c25f6f633404faeeecf21109
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-28 08:47:34 +0300

    Time: 0 ms (100.00%) | Memory: 52.5 MB (50.65%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cs
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cs
@@ -1,24 +1,26 @@
 public class Solution {
     public int MinSubArrayLen(int target, int[] nums) {
-        int result=Int32.MaxValue;
-        Queue<int> que=new();
-        int sum=0;
-        for(int i=0;i<nums.Length;i++)
-        {
-        
-                sum+=nums[i];
-                que.Enqueue(nums[i]);
-          
-             
-             while(sum>=target)
-             {
-                  result=Math.Min(result,que.Count);
-                sum-=que.Dequeue();
-             }
-            
-       
 
-        }
-        return result==Int32.MaxValue?0:result;
+	if(nums==null||nums.Length==0)
+	return 0;
+	int result=int.MaxValue;
+	int sum=0;
+	int windowbegin=0;
+	bool check=false;
+	
+    for(int i=0;i<nums.Length;i++)
+	{
+	  sum+=nums[i];
+	  
+	   while (sum >= target) {
+                result = Math.Min(result, i - windowbegin + 1);
+                sum -= nums[windowbegin];
+                windowbegin++;
+            }
     }
-}
+    
+	
+	return result==int.MaxValue?0:result;
+    }}
+        
+  
