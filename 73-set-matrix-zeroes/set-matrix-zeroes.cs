3cab164300d269157fe50b01b8d969a0c39fc3de
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-04-30 10:23:31 +0300

    Time: 3 ms (51.20%) | Memory: 53.7 MB (9.70%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cs
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cs
@@ -1,41 +1,85 @@
 public class Solution {
-    public void SetZeroes(int[][] nums) {
-	
-	HashSet<int> row=new HashSet<int>();
-	HashSet<int> column=new HashSet<int>();
-	
-	for(int i=0;i<nums.Length;i++)
-	{
-	
-		for(int j=0;j<nums[0].Length;j++)
-		{
-		if(nums[i][j]==0)
-		{
-		row.Add(i);
-		column.Add(j);
-		}
-	
-		
-		}
-
+    public void SetZeroes(int[][] matrix) {
         
-        
+        //ilk column ve ilk satırı mark bolgesi olarak kullanacagız. 
+        //Ama bunu yapmadan once buralarda kendiliğinden sıfır var mı
+        //varsa zaten burası da full sıfır olcak
 
-        
-    }
+        int row=matrix.Length;
+        int column=matrix[0].Length;
 
-    for(int i=0;i<nums.Length;i++)
-    { 
-        	for(int j=0;j<nums[0].Length;j++)
+        bool firstrow=false;
+        bool firstcolumn=false;
+
+        //1. satır ve 1. sutun kontrolü
+
+        for(int i=0;i<column;i++)
+        {
+            if(matrix[0][i]==0)
+            firstrow=true;
+        }
+        
+        for(int i=0;i<row;i++)
+        {
+            if(matrix[i][0]==0)
+            firstcolumn=true;
+        }
+
+        //2. 1. satır ve 1. sutunu işaretci olarak kullanıcaz
+        
+        for(int i=1;i<row;i++)
+        {
+             for(int j=1;j<column;j++)
             {
-                if(row.Contains(i)||column.Contains(j))
+                if(matrix[i][j]==0)
                 {
-                    nums[i][j]=0;
+                    //eleman sıfır sa git ilk satır ve ilk sutunu işaretle
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
-
+            }  
+        }
+        
+        //3. işaretli satırları ve sutunları 0 ile doldur
+         for(int i=1;i<column;i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                for(int j=1;j<row;j++)
+                {
+                    matrix[j][i]=0;
+                }
             }
+        }
+            for(int i=1;i<row;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=1;j<column;j++)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
 
- 
+        //4 son olarakta first row, firtst column 0 ile doldurulur. 
+        
+        if(firstrow)
+        {
+            for(int i=0;i<column;i++)
+        {
+           matrix[0][i]=0;
+        }
+        }
+            
+        if(firstcolumn)
+        {
+            for(int i=0;i<row;i++)
+        {
+           matrix[i][0]=0;
+        }
+        }
 
+        
     }
-}}
+}
