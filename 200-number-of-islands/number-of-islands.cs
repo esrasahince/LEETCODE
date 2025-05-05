d36d65dee8bed9bf067b48a0dd34d19cac31e191
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-05 09:37:40 +0300

    Time: 136 ms (51.88%) | Memory: 52.9 MB (41.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.cs
+++ 200-number-of-islands/number-of-islands.cs
@@ -1,37 +1,44 @@
 public class Solution {
-    
     public int NumIslands(char[][] grid) {
-
-        if(grid.Length==0)
-        return 0;
-        int row=grid.Length;
-        int column=grid[0].Length;
-        int count=0;
-
-
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<column;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                   
-                    count++;
-                    DFS(grid,i,j);
-
-                }
-            }
-        }
-        return count;        
-    }
-    public void DFS(char[][] grid,int row,int column)
+    int result=0;
+    for(int i=0;i<grid.Length;i++)
     {
-        if(row>=grid.Length||row<0||column<0||column>=grid[0].Length||grid[row][column]=='2'||grid[row][column]=='0')
-        return;
-        grid[row][column]='2';
-        DFS(grid,row-1,column);
-        DFS(grid,row+1,column);
-        DFS(grid,row,column-1);
-        DFS(grid,row,column+1);
+        for(int j=0;j<grid[0].Length;j++)
+        {
+            //yeni bir ada bulduğunda tüm adayı işaretlemeye geçiceksin.
+            //bu nedenle resultı burda artırman lazım.
+            if(grid[i][j]=='1')
+            {
+            result++;
+            GraphTraversal(grid,i,j);
+            }
+
+        }
     }
+   
+      return result;
+        
+    }
+
+   public void GraphTraversal(char[][] grid,int row, int column)
+   {
+    if(row>=grid.Length||column>=grid[0].Length||row==-1||column==-1)
+    return;
+    if(grid[row][column]!='1')
+    return;
+    //2 means visited.
+
+    grid[row][column]='2';
+  
+
+    GraphTraversal(grid,row+1,column);
+    GraphTraversal(grid,row-1,column);
+    GraphTraversal(grid,row,column+1);
+    GraphTraversal(grid,row,column-1);
+    
+
+
+
+    
+   }
 }
