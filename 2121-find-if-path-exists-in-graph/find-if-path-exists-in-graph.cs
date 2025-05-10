8ba7b10e1805c1a04205b19ddd6362a5f80d7dbe
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-10 16:12:29 +0300

    Time: 342 ms (20.34%) | Memory: 169.7 MB (23.49%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cs
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cs
@@ -1,58 +1,35 @@
-
 public class Solution {
     public bool ValidPath(int n, int[][] edges, int source, int destination) {
-
-        //I need to create a dictionary like.  vertex number and its neighbours
-        //I need a visited list
-        //for exp
-        //0: 4,9
-        //1: 4,7
-        //2: 4
-        //3: 4
-        //4: 3,1,8,6,2,7,0,5
-        //5: 4
-        //6: 4,
-        //7:1,4
-        //8:4,
-        //9:0
-        // for loop 
-        //ınvestigate all child whether having dest or not
-        if(edges.Length==0||edges==null||source==destination)
-        return true;
-        
-        Dictionary<int,HashSet<int>> edge=new();
+        Dictionary<int,List<int>> graph=new();
         for(int i=0;i<n;i++)
         {
-                edge[i]=new HashSet<int>();
+            graph[i]=new List<int>();
         }
-        
-        for(int i=0;i<edges.Length;i++)
+        foreach(var edge in edges)
         {
-            edge[edges[i][0]].Add(edges[i][1]);
-            edge[edges[i][1]].Add(edges[i][0]);
+            graph[edge[0]].Add(edge[1]);
+            graph[edge[1]].Add(edge[0]);
         }
         HashSet<int> visited=new();
-        return Check(edge,source,destination,visited);
-        
+        return DFS(graph,source,destination,visited);
+        //undirected graph
         
     }
-    public bool Check(Dictionary<int,HashSet<int>> edge, int source,int dest, HashSet<int> visited )
-   {
-    if(edge[source].Contains(dest))
-    return true;
-    visited.Add(source);
-    foreach(int value in edge[source])
+    public bool DFS(Dictionary<int,List<int>> graph, int current,int dest,HashSet<int> visited)
     {
-        
-                if(!visited.Contains(value))
-                {
-                
-                     if (Check(edge, value, dest, visited))
-                    return true;
-                }       
-            
+        if(current==dest)
+        return true;
+        if(visited.Contains(current))
+        return false;
+        visited.Add(current);
+        foreach(int i in graph[current])
+        {
+            if(!visited.Contains(i))
+            {
+                if(DFS(graph,i,dest,visited))
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-
-   } 
 }
