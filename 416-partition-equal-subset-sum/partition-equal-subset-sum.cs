5f842fc606e55bbacf8c5f81db583ec404c4f32b
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-16 15:45:21 +0300

    Time: 233 ms (21.56%) | Memory: 77.6 MB (10.20%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cs
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cs
@@ -1,42 +1,55 @@
 public class Solution {
     public bool CanPartition(int[] nums) {
-        int totalsum=0;
-        for(int i=0;i<nums.Length;i++)
-        {
-            totalsum+=nums[i];
-
+        // Calculate total sum of all elements
+        int totalSum = 0;
+        foreach (int num in nums) {
+            totalSum += num;
         }
-        if(totalsum%2!=0)
-        return false;
-        return BottomUp(nums,totalsum/2);
         
+        // If totalSum is odd, we cannot partition into two equal subsets
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+        
+        // We need to find if a subset can sum up to totalSum/2
+        int targetSum = totalSum / 2;
+        
+        // Use a dictionary to memoize our results
+        // Key: (index, currentSum), Value: bool result
+        Dictionary<(int, int), bool> memo = new Dictionary<(int, int), bool>();
+        
+        return TopDown(nums, nums.Length - 1, targetSum, memo);   
     }
-     public bool BottomUp(int[] nums,int sum)
-    { 
-
-        bool[,] matrix=new bool[nums.Length+1,sum+1];
-          // If sum is 0, then answer is true (empty subset)
-        for (int i = 0; i <= nums.Length; i++) {
-            matrix[i, 0] = true;
+    
+    public bool TopDown(int[] nums, int index, int remainingSum, Dictionary<(int, int), bool> memo) {
+        // Base cases
+        if (remainingSum == 0) {
+            return true; // We've found a valid subset
         }
-
-        for(int i=1;i<nums.Length+1;i++)  //index
-        {
-            for(int j=1;j<sum+1;j++)  //sum
-            {
-               if(nums[i-1]>j)
-               {
-                matrix[i,j]=matrix[i-1,j];
-               }
-               else
-               {
-                matrix[i,j]=matrix[i-1,j]||matrix[i-1,j-nums[i-1]];
-
-               }
-            }
+        
+        if (index < 0 || remainingSum < 0) {
+            return false; // We've either used all items or exceeded our target sum
         }
-        return matrix[nums.Length,sum];
-
-
+        
+        // Check if we've already computed this state
+        if (memo.ContainsKey((index, remainingSum))) {
+            return memo[(index, remainingSum)];
+        }
+        
+        // Decision: either include the current element or exclude it
+        bool result;
+        
+        // If current element is larger than the remaining sum, we can only exclude it
+        if (nums[index] > remainingSum) {
+            result = TopDown(nums, index - 1, remainingSum, memo);
+        } else {
+            // Try both including and excluding the current element
+            result = TopDown(nums, index - 1, remainingSum - nums[index], memo) || 
+                    TopDown(nums, index - 1, remainingSum, memo);
+        }
+        
+        // Memoize the result
+        memo[(index, remainingSum)] = result;
+        return result;
     }
 }
