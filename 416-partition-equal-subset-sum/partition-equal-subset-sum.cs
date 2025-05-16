e7155cadaef1013700943f82fca22599611ef37a
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-16 15:37:30 +0300

    Time: 58 ms (36.20%) | Memory: 51.2 MB (40.95%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cs
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cs
@@ -1,39 +1,42 @@
 public class Solution {
     public bool CanPartition(int[] nums) {
-
-        //Input: nums = [1,5,11,5] Output: true
-        //The array can be partitioned as [1, 5, 5] and [11].
-
-        //output boolean
-        //choices for each item add first group, or add second group not include at the same time
-        //input--> index sum1, sum2
-        //index<0 if(sum1==sum2) true else false
-        Dictionary<(int,int),bool> memo=new();
-        int sum=0;
+        int totalsum=0;
         for(int i=0;i<nums.Length;i++)
         {
-            sum+=nums[i];
+            totalsum+=nums[i];
 
         }
-        if(sum%2!=0)
+        if(totalsum%2!=0)
         return false;
-        
-        return TopDown(nums,nums.Length-1,sum/2,memo);
-
+        return BottomUp(nums,totalsum/2);
         
     }
-    public bool TopDown(int[] nums,int index,int sum1, Dictionary<(int,int),bool> memo)
-    {
-        if(index<0)
-        {
-            return sum1==0?true:false;
-        }
-        if(memo.ContainsKey((index,sum1)))
-        {
-            return memo[(index,sum1)];
+     public bool BottomUp(int[] nums,int sum)
+    { 
+
+        bool[,] matrix=new bool[nums.Length+1,sum+1];
+          // If sum is 0, then answer is true (empty subset)
+        for (int i = 0; i <= nums.Length; i++) {
+            matrix[i, 0] = true;
         }
 
-        memo[(index,sum1)]=TopDown(nums,index-1,sum1-nums[index],memo)||TopDown(nums,index-1,sum1,memo);
-        return memo[(index,sum1)];
+        for(int i=1;i<nums.Length+1;i++)  //index
+        {
+            for(int j=1;j<sum+1;j++)  //sum
+            {
+               if(nums[i-1]>j)
+               {
+                matrix[i,j]=matrix[i-1,j];
+               }
+               else
+               {
+                matrix[i,j]=matrix[i-1,j]||matrix[i-1,j-nums[i-1]];
+
+               }
+            }
+        }
+        return matrix[nums.Length,sum];
+
+
     }
 }
