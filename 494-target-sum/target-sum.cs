791f54d54d980c0a28a0e9e4fafc1b52a2b0b444
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-16 08:35:43 +0300

    Time: 137 ms (54.54%) | Memory: 47.3 MB (45.06%) - LeetSync

--- 494-target-sum/target-sum.cs
+++ 494-target-sum/target-sum.cs
@@ -1,25 +1,44 @@
 public class Solution {
     public int FindTargetSumWays(int[] nums, int target) {
-      
-       int n= nums.Length;
-       Dictionary<(int, int), int> memo = new();
-       return TopDown(nums,n-1,target,memo);
-    }
-    public int TopDown(int[] nums, int ind, int target,Dictionary<(int, int), int> memo )
-    {
-       if(ind < 0)
-        return target == 0 ? 1 : 0;//hic eleman kalmamossa ve target 0 a ulasmıssın count 1 artacak.
-        if(memo.ContainsKey((ind,target)))
-        return memo[(ind,target)];
+
+        //Elemanın başına + ya da - koyarak target suma ulaşmak istiyoruz
+        //kaç farklı şekilde yapabiliz yani count soruyor 
+
+        //1. Count sorduğu için output int olmalı
+        //2. inputlar-> nums,index,currentsum,memo  burada değişenler currentsum ve index 
+        //bu nedenle tüm senaryoyu bunlara göre yapacağız.
+        //BASE KOŞUL
+        //dizide hiç eleman kalmadığında yani index<0 iken
+        //currentsum da sıfırsa istediğimiz duruma ulaşmışız demektir count 1 olmalı
+        //currentsum sıfırdan farklı ise istediğimiz koşula ulaşamadık. 0 dön
+        //CHOICE DIAGRAM
+        //Burada her bir item için ekle yada çıkar mantığı var.
+        //peki ekleme çıkarma için bir önkoşul var mı? currentsumdan küçükse büyükse ekle gibi.
+        //HAYIR elemanları sıralı bir şekilde yalnızca + ve - ile yönetiyoruz. tamamen çıkarmıyoruz.
+
+        //MEMORIZATION
+        //değişen elemanlar currentsum ve index olduğu için bunları memorize ediyoruz
+        Dictionary<(int,int),int> memo=new();
+        return TopDown(nums,target,nums.Length-1,memo);
+
         
-       
-            //ekle 
-           memo[(ind, target)] = TopDown(nums, ind - 1, target - nums[ind], memo)
-                    + TopDown(nums, ind - 1, target + nums[ind], memo);
+    }
+    public int TopDown(int[] nums,int currentsum,int index, Dictionary<(int,int),int> memo)
+    {
+        //BASE KOŞUL
+        if(index<0)
+        {
+            return currentsum==0?1:0;
+        }
+        if(memo.ContainsKey((currentsum,index)))
+        return memo[(currentsum,index)];
 
+        //+ koşulu
+        int plus=TopDown(nums,currentsum+nums[index],index-1,memo);
+        int minus=TopDown(nums,currentsum-nums[index],index-1,memo);
+        memo[(currentsum,index)]=plus+minus;
 
-       
-         return memo[(ind,target)];
+        return plus+minus;
 
     }
 }
