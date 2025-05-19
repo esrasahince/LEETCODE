a6998f8535e4a3227a8f31567391d00b747fef0e
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-19 07:53:29 +0300

    Time: 40 ms (5.88%) | Memory: 46.1 MB (14.71%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cs
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cs
@@ -1,23 +1,28 @@
 public class Solution {
     public int LastStoneWeightII(int[] stones) {
-        Dictionary<(int,int,int),int> memo=new();
-        return TopDown(stones,stones.Length-1,0,0,memo);
+        Dictionary<(int,int),int> memo=new();
+        int totalsum=0;
+        for(int i=0;i<stones.Length;i++)
+        {
+            totalsum+=stones[i];
 
-
-
-        
+        }
+        return TopDown(stones,stones.Length-1,0,totalsum,memo);
     }
-    public int TopDown(int[] numbers,int index,int sum1,int sum2,Dictionary<(int,int,int),int> memo)
+    public int TopDown(int[] numbers,int index,int sum,int totalsum,Dictionary<(int,int),int> memo)
     {
         if(index<0)
-        return Math.Abs(sum1-sum2);
-        if(memo.ContainsKey((index,sum1,sum2)))
-        return memo[(index,sum1,sum2)];
+        return Math.Abs(2*sum-totalsum);
+        if(memo.ContainsKey((index,sum)))
+        return memo[(index,sum)];
 
-       int first=TopDown(numbers,index-1,sum1+numbers[index],sum2,memo);
-       int second=TopDown(numbers,index-1,sum1,sum2+numbers[index],memo);
-       memo[(index,sum1,sum2)]=Math.Min(first,second);
-       return  memo[(index,sum1,sum2)];
+       int first=TopDown(numbers,index-1,sum+numbers[index],totalsum,memo);
+       int second=TopDown(numbers,index-1,sum,totalsum,memo);
+       memo[(index,sum)]=Math.Min(first,second);
+       return  memo[(index,sum)];
 
     }
-}
+   
+
+
+    }
