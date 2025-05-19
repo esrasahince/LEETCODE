7ad9c01b3930abc8a025e8cb3a095c221e127bbe
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-19 08:26:56 +0300

    Time: 3 ms (94.12%) | Memory: 41.7 MB (82.35%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cs
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cs
@@ -1,28 +1,49 @@
 public class Solution {
-    public int LastStoneWeightII(int[] stones) {
-        Dictionary<(int,int),int> memo=new();
-        int totalsum=0;
-        for(int i=0;i<stones.Length;i++)
-        {
-            totalsum+=stones[i];
 
-        }
-        return TopDown(stones,stones.Length-1,0,totalsum,memo);
-    }
-    public int TopDown(int[] numbers,int index,int sum,int totalsum,Dictionary<(int,int),int> memo)
-    {
-        if(index<0)
-        return Math.Abs(2*sum-totalsum);
-        if(memo.ContainsKey((index,sum)))
-        return memo[(index,sum)];
-
-       int first=TopDown(numbers,index-1,sum+numbers[index],totalsum,memo);
-       int second=TopDown(numbers,index-1,sum,totalsum,memo);
-       memo[(index,sum)]=Math.Min(first,second);
-       return  memo[(index,sum)];
-
-    }
+   public int LastStoneWeightII(int[] stones) {
+   int totalsum=0;
+   int n=stones.Length;
    
-
-
-    }
+   for(int i=0;i<n;i++)
+   {
+   totalsum+=stones[i];
+   }
+   
+   //goal 
+   int sum=totalsum/2;
+   bool[,] dp=new bool[n+1,sum+1];
+   
+   //sum 0 iken stones dizisindeki eleman sayısı ne olursa olsun toplamı 0 olan subset oluşturabiliri.
+   //Dikkat stoneslardan biri 0 olsaydı bu mantık boyle işlemezdi
+   for(int i=0;i<n+1;i++)
+   {
+   dp[i,0]=true;
+   }
+   for(int i=1;i<n+1;i++)
+   {
+     for(int j=1;j<sum+1;j++)
+     {
+        if(stones[i-1]>j) //dont include
+        { 
+         dp[i,j]=dp[i-1,j];
+        }
+        else
+        {
+        dp[i,j]=dp[i-1,j]||dp[i-1,j-stones[i-1]];
+        }
+     }
+   }
+   
+   //amacımız s1 in aslında totalsumın yarısına en yakın halini bulmak
+   //yani yukarıdaki tabloda dp tablosunun en alttaki satırında j= totalsum/2 ye en yakın true değeri bize 
+   //s1 sum alacağı maxımumu verir.
+   int s1sum=0;
+   for(int i=0;i<=sum;i++) 
+   { 
+   if(dp[n,i])
+        s1sum=i;
+   
+   }
+   
+   return Math.Abs(totalsum-(2*s1sum));
+   }}
