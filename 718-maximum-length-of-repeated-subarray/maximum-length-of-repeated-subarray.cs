fc86a1d9c8553863ff557003befff7cdec9d2d84
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-22 09:38:32 +0300

    Time: 28 ms (95.35%) | Memory: 57.9 MB (16.28%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cs
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cs
@@ -4,22 +4,25 @@ public class Solution {
         int n=nums1.Length;
         int m=nums2.Length;
         
-        int[,] dp=new int[n+1,m+1];
+        int[] prev=new int[m+1];
+        
         int max=0;
         for(int i=1;i<n+1;i++)
         {
+            int[] current=new int[m+1];
             for(int j=1;j<m+1;j++)
             {
                 if(nums1[i-1]==nums2[j-1])
                 {
-                    dp[i,j]=1+dp[i-1,j-1];
-                    max=Math.Max(max,dp[i,j]);
+                    current[j]=1+prev[j-1];
+                    max=Math.Max(max,current[j]);
                 }
                 else
                 {
-                    dp[i,j]=0;
+                    current[j]=0;
                 }
             }
+            prev=current;
         }
         return max;
         
