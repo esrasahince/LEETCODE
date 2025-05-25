43dd451328f6bc53e928e315563e9af767990adc
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-25 13:40:22 +0300

    Time: 0 ms (100.00%) | Memory: 42.5 MB (63.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,49 +1,41 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        int minindex=FindIndexOfMinimum(nums);
-        Console.Write(minindex);
-        int firstpart=BinarySearch(nums,target,minindex,nums.Length-1);
-       if(firstpart!=-1)
-       return firstpart;
 
-        int secondpart=BinarySearch(nums,target,0,minindex-1);
-         if(secondpart!=-1)
-       return secondpart;
+        int left = 0, right = nums.Length - 1;
 
-       return -1;
+        while (left <=right) {
+            int middle = left + (right - left) / 2;
 
-        
-    }
-    public int FindIndexOfMinimum(int[] nums)
-    {
-        int left=0;
-        int right=nums.Length-1;
-        while(left<right)
-        {
-            if(nums[left]<nums[right])
-            return left;
-            int middle=left+(right-left)/2;
-            if(nums[middle]>nums[right])
-            left=middle+1;
-            else if(nums[middle]<nums[left])
-            right=middle;
+            if (nums[middle] == target)
+                return middle;
+
+            // Sağ taraf sıralıysa
+            if (nums[middle] <=nums[right]) {
+                if (target > nums[middle] && target <= nums[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+         
+
+            // Sol taraf sıralıysa
+            else if (nums[middle] >= nums[left]) {
+                if (target >= nums[left] && target < nums[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            }
+            
+
+            // Belirsizlik varsa uçları daralt
+          
         }
-        return left;
-    }
-    public int BinarySearch(int[] nums,int target, int left,int right)
-    {
-        
-        while(left<=right)
-        {
-            int middle=left+(right-left)/2;
-            if(nums[middle]==target)
-            return middle;
-            if(nums[middle]<target)
-            left=middle+1;
-            else
-            right=middle-1;
-        }
+
         return -1;
-
     }
 }
+
+        
+    
