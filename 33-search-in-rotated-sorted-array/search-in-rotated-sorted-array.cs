c40157b15fb515ea8400dcbdad889e0f422ca1d3
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-05-25 11:08:31 +0300

    Time: 1 ms (9.14%) | Memory: 42.4 MB (75.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,42 +1,49 @@
 public class Solution {
     public int Search(int[] nums, int target) {
+        int minindex=FindIndexOfMinimum(nums);
+        Console.Write(minindex);
+        int firstpart=BinarySearch(nums,target,minindex,nums.Length-1);
+       if(firstpart!=-1)
+       return firstpart;
+
+        int secondpart=BinarySearch(nums,target,0,minindex-1);
+         if(secondpart!=-1)
+       return secondpart;
+
+       return -1;
+
+        
+    }
+    public int FindIndexOfMinimum(int[] nums)
+    {
         int left=0;
         int right=nums.Length-1;
-        int middle=0;
-         while(left<=right)
-            {
-            middle=left+(right-left)/2;
-
-                //no rotate
-                if(nums[middle]==target)
-                return middle;
-                
-            if(nums[left]<=nums[middle])
-            {
-
-                if(nums[left]<=target&&target<nums[middle])
-                right=middle-1;
-                else 
-                left=middle+1;
-            }
-            else  if(nums[right]>=nums[middle])
-            {
-               
-
-                if(nums[right]>=target&&target>nums[middle])
-                left=middle+1;
-                else 
-                right=middle-1;
-               
-            }
-           
-        
+        while(left<right)
+        {
+            if(nums[left]<nums[right])
+            return left;
+            int middle=left+(right-left)/2;
+            if(nums[middle]>nums[right])
+            left=middle+1;
+            else if(nums[middle]<nums[left])
+            right=middle;
         }
+        return left;
+    }
+    public int BinarySearch(int[] nums,int target, int left,int right)
+    {
         
-         return -1;}
+        while(left<=right)
+        {
+            int middle=left+(right-left)/2;
+            if(nums[middle]==target)
+            return middle;
+            if(nums[middle]<target)
+            left=middle+1;
+            else
+            right=middle-1;
+        }
+        return -1;
 
     }
-
-
-        
-    
+}
