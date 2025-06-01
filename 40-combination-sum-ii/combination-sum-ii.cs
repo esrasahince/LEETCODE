9d064f72705e1c6eb28af58851e6c3828be7b676
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-01 10:11:05 +0300

    Time: 8 ms (40.52%) | Memory: 48.1 MB (17.54%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cs
+++ 40-combination-sum-ii/combination-sum-ii.cs
@@ -3,14 +3,13 @@ public class Solution {
         Array.Sort(candidates);
         List<int> temp=new();
         List<IList<int>> result=new();
-        bool[] visited=new bool[candidates.Length];
-        Backtracking(candidates,visited,0,0,target,temp,result);
+        Backtracking(candidates,0,0,target,temp,result);
         
 
         return result;
         
     }
-    public void Backtracking(int[] nums,bool[] visited,int start,int sum,int target,List<int> temp,List<IList<int>> result)
+    public void Backtracking(int[] nums,int start,int sum,int target,List<int> temp,List<IList<int>> result)
     {
         if(sum==target)
         {
@@ -25,15 +24,15 @@ public class Solution {
         for(int i=start;i<nums.Length;i++)
         { //1,1,2,2
           
-            if(i>start&&nums[i]==nums[i-1]&&visited[i]==false)
+            if(i>start&&nums[i]==nums[i-1])
             continue;
        
 
             temp.Add(nums[i]);
-            visited[i]=true;
-            Backtracking(nums,visited,i+1,sum+nums[i],target,temp,result);
+      
+            Backtracking(nums,i+1,sum+nums[i],target,temp,result);
             temp.RemoveAt(temp.Count-1);
-            visited[i]=false;
+           
         }
     }
 }
