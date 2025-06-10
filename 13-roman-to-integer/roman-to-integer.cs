799ec0dae94412c7216bbe275851a9818eccdfa9
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-10 17:38:41 +0300

    Time: 3 ms (71.66%) | Memory: 51 MB (23.07%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cs
+++ 13-roman-to-integer/roman-to-integer.cs
@@ -8,10 +8,7 @@ public class Solution {
         roman['C']=100; //D,M
         roman['D']=500;
         roman['M']=1000;
-       Dictionary<char,HashSet<char>> rules=new();
-       rules['I']=new HashSet<char>(){'V','X'};
-       rules['X']=new HashSet<char>(){'L','C'};
-       rules['C']=new HashSet<char>(){'D','M'};
+
 
         
         //rule 1: XX + V + II=27, same char number
@@ -21,28 +18,21 @@ public class Solution {
         int sum=0;
         for(int i=0;i<s.Length;i++)
         {
-            char item=s[i];
-           if(rules.ContainsKey(s[i]))
-           {
-              if(i+1<s.Length)
-              {
-                if(rules[item].Contains(s[i+1]))
-                {
-                    sum+=roman[s[i+1]]-roman[s[i]];
-                    i++;
-                    continue;
-                }
-              }
-                
-                    sum+=roman[s[i]];
-                
+            if(i+1<s.Length&&roman[s[i]]<roman[s[i+1]])
+            {
+                  sum-=roman[s[i]];
+                  continue;
+               
+
+            }
+             sum+=roman[s[i]];
+             
+            
               
-           }
-           else
-           {
-            sum+=roman[s[i]];
-           }
+            
         }
+
+
         return sum;
 
 
