aba4dc6aad3a97b2e23254e3d0b5fa3547672cb6
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-11 23:15:18 +0300

    Time: 135 ms (51.02%) | Memory: 53.9 MB (27.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.cs
+++ 200-number-of-islands/number-of-islands.cs
@@ -1,65 +1,67 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        int island=0;
+        int n=grid.Length;
+        int m=grid[0].Length;
 
+        int[] parent=new int[n*m+m];
+        int[] size=new int[n*m+m];
+        for(int i=0;i<n*m+m;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+        int onecount=0;
+        int component=0;
         for(int i=0;i<grid.Length;i++)
         {
             for(int j=0;j<grid[0].Length;j++)
             {
                 if(grid[i][j]=='1')
-                {
-                    BFS(grid,i,j);
-                    island++;
-                }
+                {   onecount++;
+                    int current=i*m+j;
+                    int near=0;
+              
+                        if(j+1<m&&grid[i][j+1]=='1')
+                    {
+                        near=(i)*m+j+1;
+                        Union(parent,size,current,near,ref component);
 
+                    }
+                          if(i+1<n&&grid[i+1][j]=='1')
+                    {
+                        near=(i+1)*m+j;
+                        Union(parent,size,current,near,ref component);
+
+                    }
+                    
+                }
             }
         }
-        return island;
+        return onecount-component;
         
     }
-    public void DFS(char[][] grid,int row,int column)
+    public int Find(int[] parent,int x)
     {
-        if(row<0||column<0||row>=grid.Length||column>=grid[0].Length)
-        return;
-        if(grid[row][column]=='2'||grid[row][column]=='0')
-        return;
-        grid[row][column]='2';
-        DFS(grid,row+1,column);
-        DFS(grid,row-1,column);
-        DFS(grid,row,column+1);
-        DFS(grid,row,column-1);
+        if(parent[x]==x)
+        return x;
+        parent[x]=Find(parent,parent[x]);
+        return parent[x];
     }
-
-     
-    public void BFS(char[][] grid,int row,int column)
-{
-    Queue<(int,int)> que=new();
-    que.Enqueue((row,column));
-    grid[row][column]='2';  // Mark as visited
-    
-    while(que.Count>0)
+    public void Union(int[] parent,int[] size,int x,int y,ref int component)
     {
-        var (r,c)=que.Dequeue();
-        
-        // Check all 4 directions
-        if(r-1>=0 && grid[r-1][c]=='1') {
-            grid[r-1][c]='2';  // Mark as visited when enqueuing
-            que.Enqueue((r-1,c));
-        }
-        if(r+1<grid.Length && grid[r+1][c]=='1') {
-            grid[r+1][c]='2';
-            que.Enqueue((r+1,c));
-        }
-        if(c-1>=0 && grid[r][c-1]=='1') {
-            grid[r][c-1]='2';
-            que.Enqueue((r,c-1));
-        }
-        if(c+1<grid[0].Length && grid[r][c+1]=='1') {
-            grid[r][c+1]='2';
-            que.Enqueue((r,c+1));
-        }
-    }
+        int parent1=Find(parent,x);
+        int parent2=Find(parent,y);
+        if(parent1==parent2)
+         return;
+        if (size[parent1] < size[parent2]) {
+            parent[parent1] = parent2;
+            size[parent2] += size[parent1];
+        } else {
+            parent[parent2] = parent1;
+            size[parent1] += size[parent2];
 }
 
+        component++;
 
+    }
 }
