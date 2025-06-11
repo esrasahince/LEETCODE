53d0ec977aac060b301a5c8c963f0b30b78a1007
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-11 20:08:45 +0300

    Time: 132 ms (62.96%) | Memory: 53.2 MB (38.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.cs
+++ 200-number-of-islands/number-of-islands.cs
@@ -1,44 +1,65 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-    int result=0;
-    for(int i=0;i<grid.Length;i++)
-    {
-        for(int j=0;j<grid[0].Length;j++)
-        {
-            //yeni bir ada bulduğunda tüm adayı işaretlemeye geçiceksin.
-            //bu nedenle resultı burda artırman lazım.
-            if(grid[i][j]=='1')
-            {
-            result++;
-            GraphTraversal(grid,i,j);
-            }
+        int island=0;
 
+        for(int i=0;i<grid.Length;i++)
+        {
+            for(int j=0;j<grid[0].Length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    BFS(grid,i,j);
+                    island++;
+                }
+
+            }
         }
-    }
-   
-      return result;
+        return island;
         
     }
+    public void DFS(char[][] grid,int row,int column)
+    {
+        if(row<0||column<0||row>=grid.Length||column>=grid[0].Length)
+        return;
+        if(grid[row][column]=='2'||grid[row][column]=='0')
+        return;
+        grid[row][column]='2';
+        DFS(grid,row+1,column);
+        DFS(grid,row-1,column);
+        DFS(grid,row,column+1);
+        DFS(grid,row,column-1);
+    }
 
-   public void GraphTraversal(char[][] grid,int row, int column)
-   {
-    if(row>=grid.Length||column>=grid[0].Length||row==-1||column==-1)
-    return;
-    if(grid[row][column]!='1')
-    return;
-    //2 means visited.
-
-    grid[row][column]='2';
-  
-
-    GraphTraversal(grid,row+1,column);
-    GraphTraversal(grid,row-1,column);
-    GraphTraversal(grid,row,column+1);
-    GraphTraversal(grid,row,column-1);
+     
+    public void BFS(char[][] grid,int row,int column)
+{
+    Queue<(int,int)> que=new();
+    que.Enqueue((row,column));
+    grid[row][column]='2';  // Mark as visited
     
+    while(que.Count>0)
+    {
+        var (r,c)=que.Dequeue();
+        
+        // Check all 4 directions
+        if(r-1>=0 && grid[r-1][c]=='1') {
+            grid[r-1][c]='2';  // Mark as visited when enqueuing
+            que.Enqueue((r-1,c));
+        }
+        if(r+1<grid.Length && grid[r+1][c]=='1') {
+            grid[r+1][c]='2';
+            que.Enqueue((r+1,c));
+        }
+        if(c-1>=0 && grid[r][c-1]=='1') {
+            grid[r][c-1]='2';
+            que.Enqueue((r,c-1));
+        }
+        if(c+1<grid[0].Length && grid[r][c+1]=='1') {
+            grid[r][c+1]='2';
+            que.Enqueue((r,c+1));
+        }
+    }
+}
 
 
-
-    
-   }
 }
