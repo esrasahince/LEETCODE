59ba82055f114b3269e8795a83f8bd36a97ed25f
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-25 14:01:10 +0300

    Time: 0 ms (100.00%) | Memory: 41.9 MB (90.91%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
@@ -1,30 +1,27 @@
 public class Solution {
     public int FindMin(int[] nums) {
+         int left = 0;
+    int right = nums.Length - 1;
+    while (left < right)
+    {
+        int middle = left + (right - left) / 2;
+        //koşul nedir
+        //middle < right olmalı. burdaki durumda 
+        //middle>right
 
-        if(nums.Length==1)
-        return nums[0];
-        int left=0;
-        int right=nums.Length-1;
-        while(left<right)
+        if(nums[middle]>nums[right])
         {
-            if(nums[left]<=nums[right])
-            return nums[left];
-
-            int middle=left+(right-left)/2;
-        
-
-            if(nums[middle]>nums[right])
-            {
-                left=middle+1;
-            }
-            else if(nums[middle]<nums[left])
-            {
-                right=middle;
-            }
-
+          left=middle+1;
         }
-        return nums[left];
+        else
+        {
+            right=middle;
+        }
 
+    }
+    //left=rigth oldugunda dongu biter.
+    //left kosulu saglayan ilk elemanı  gosterir 
+    return nums[left];
         
     }
 }
