2bcc9efeda109afb64a68dfb381c373c3689ce4d
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-26 19:28:04 +0300

    Time: 0 ms (100.00%) | Memory: 43.5 MB (23.39%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -1,22 +1,18 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        int left=0;
-        int right=nums.Length-1;
-        while(left<=right)
-        {
-            int middle=left+(right-left)/2;
-            if(nums[middle]==target)
-            return middle;
-            if(target>nums[middle])
-            {
-                left=middle+1;
-
-            }
-            else
-            right=middle-1;
-        }
-        return left;
-        
+    int left=0;
+    int right=nums.Length-1;
+	while(left<=right)
+	{
+		int middle=left+(right-left)/2;
+		if(nums[middle]==target)
+			return middle;
+		if(nums[middle]<target)
+			left=middle+1;
+		else
+			right=middle-1; 
+	}
+	return right+1;
         
     }
 }
