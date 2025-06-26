0453fc17874c808d8ef5f00534e70ec28dba24f5
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-26 19:49:11 +0300

    Time: 0 ms (100.00%) | Memory: 29.2 MB (42.40%) - LeetSync

--- 69-sqrtx/sqrtx.cs
+++ 69-sqrtx/sqrtx.cs
@@ -2,9 +2,6 @@ public class Solution {
     public int MySqrt(int x) {
         if(x==0|| x==1)
         return x;
-    
-        
-
         int left=0;
         int right=x/2;
       while(left<=right)
@@ -12,12 +9,15 @@ public class Solution {
             int middle=left+(right-left)/2;
             if(x==(long)middle*middle)
             return middle;
-            else if( x<(long)middle*middle)
-            right=middle-1;
-            else
+            else if( x>(long)middle*middle)
             left=middle+1;
-        }
-        return right;
+            else
+            right=middle-1;
+      }
+      //right< left oldugunda sorgu bitti.
+      //right-->target-->left şeklinde
+      return right;
+       
         
     }
 }
