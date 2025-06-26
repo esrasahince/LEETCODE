6803cb52a4e9917ce5e655be1e71114415a7bd3e
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-26 20:38:59 +0300

    Time: 0 ms (100.00%) | Memory: 43 MB (8.88%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,41 +1,40 @@
 public class Solution {
     public int Search(int[] nums, int target) {
 
-        int left = 0, right = nums.Length - 1;
+        int left=0;
+        int right=nums.Length-1;
+        while(left<=right)
+        {
+            int middle=left+(right-left)/2;
+            if(nums[middle]==target)
+            return middle;
+            //hangi taraf sıralı
+            if(nums[middle]<=nums[right])
+            { //sağ taraf sıralı
+               if(nums[middle]<target&&target<=nums[right])
+               { 
+                left=middle+1;
+               }
+               else
+               {
+                right=middle-1;
+               }
 
-        while (left <=right) {
-            int middle = left + (right - left) / 2;
-
-            if (nums[middle] == target)
-                return middle;
-
-            // Sağ taraf sıralıysa
-            if (nums[middle] <=nums[right]) {
-                if (target > nums[middle] && target <= nums[right]) {
-                    left = middle + 1;
-                } else {
-                    right = middle - 1;
-                }
             }
-         
+            else
+            { //sol taraf sıralı
+              if(nums[middle]>target&&target>=nums[left])
+               { 
+                right=middle-1;
+               }
+               else
+               {
+                left=middle+1;
+               }
 
-            // Sol taraf sıralıysa
-            else if (nums[middle] >= nums[left]) {
-                if (target >= nums[left] && target < nums[middle]) {
-                    right = middle - 1;
-                } else {
-                    left = middle + 1;
-                }
             }
-            
-
-            // Belirsizlik varsa uçları daralt
-          
         }
-
         return -1;
+        
     }
 }
-
-        
-    
