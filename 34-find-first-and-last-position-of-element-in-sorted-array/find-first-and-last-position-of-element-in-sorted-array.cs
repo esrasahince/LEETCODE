0703329cc4ee4057c62a931e4fcd3bd7a9b49dcb
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-26 20:15:27 +0300

    Time: 0 ms (100.00%) | Memory: 49.9 MB (57.26%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,65 +1,49 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        if(nums.Length==1&&target==nums[0])
-         return new int[2];
-        if(nums==null||nums.Length==0)
-           return new int[]{-1,-1};
+        if(nums==null||nums.Length<1)
+        return new int[2]{-1,-1};
         
-        int[] result=new int[2];
-        result[0]=FindFirst(nums,target);
-        result[1]=FindLast(nums,target);
-        
-     return result;
+        int first=FirstPosition(nums,target);
+        int last=LastPosition(nums,target);
+        return new int[2]{first,last};
         
     }
+    public int FirstPosition(int[] nums,int target)
+    {
+        int left=0;
+        int right=nums.Length-1;
+        while(left<right)
+        {
+            int middle=left+(right-left)/2;
+           
+            if(nums[middle]<target)
+            left=middle+1;
+            else
+            right=middle;
+        }
+        //left==right durumunda döngü bitti
+        return nums[left]==target?left:-1;
 
-    public int FindLast(int[] nums, int target)
+    }
+     public int LastPosition(int[] nums,int target)
     {
         int left=0;
         int right=nums.Length-1;
-        int result=-1;
+        int lastoccurence=-1;
         while(left<=right)
         {
             int middle=left+(right-left)/2;
-            if(nums[middle]==target)
-            { 
-                result=middle;
-                left=middle+1;
-                
-            }
           
-            else if(nums[middle]<target)
+            if(nums[middle]==target)
             {
+                lastoccurence=middle;
                 left=middle+1;
             }
+            else if(nums[middle]<target)
+            left=middle+1;
             else
             right=middle-1;
         }
-        return result;
-    }
-    public int FindFirst(int[] nums, int target)
-    {
-        int left=0;
-        int right=nums.Length-1;
-        int result=-1;
-        while(left<=right)
-        {
-            int middle=left+(right-left)/2;
-            if(nums[middle]==target)
-            { 
-                result=middle;
-                right=middle-1;
-                
-            }
-          
-            else if(nums[middle]<target)
-            {
-                left=middle+1;
-            }
-            else
-            right=middle-1;
-        }
-        return result;
-    }
-    
-}
+        
+        return lastoccurence;
+}}
