2a054ddcba5cfc809b191e431c7c366464258677
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-26 22:20:48 +0300

    Time: 26 ms (41.68%) | Memory: 28.9 MB (64.22%) - LeetSync

--- 278-first-bad-version/first-bad-version.cs
+++ 278-first-bad-version/first-bad-version.cs
@@ -3,26 +3,24 @@
 
 public class Solution : VersionControl {
     public int FirstBadVersion(int n) {
+        int left=1;
+        int right=n;
+        while(left<right)
+        {
+            int middle=left+(right-left)/2;
+            if(!IsBadVersion(middle))
+            {
+                left=middle+1;
+            }
+            else
+            {
+                right=middle;
+            }
+        }
+        //left=right oldugunda dongu biter.
+        return left;
 
-        //elimizde n tane versiyon var. 
-        // bir yerden sonra işler karışmış.
-        //işlerin  İLK NEREDE karıştığını bulmaya çalışıyoruz.
-        //problemi log n zamanda çözmemiz gerekiyor
-    int left = 0;
-    int right = n;
-    int result = -1;
 
-    while (left <= right) {
-        int middle = left + (right - left) / 2;
-
-        if (IsBadVersion(middle)) {
-            result = middle;     // Eşleşme bulundu
-            right = middle - 1;   // Sağda daha ileri bir eşleşme olabilir
-        } else
-            left = middle + 1;   // Sağ tarafa git
-     
+        
     }
-
-    return result;
-}
 }
