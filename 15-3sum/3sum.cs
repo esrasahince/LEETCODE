221244cf65f6eec809ea3572336b34e17454794c
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-06-30 19:51:11 +0300

    Time: 74 ms (20.28%) | Memory: 67.3 MB (16.44%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,43 +1,41 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
+        
         Array.Sort(nums);
-        List<IList<int>> result=new();
-
-        HashSet<(int,int,int)> set=new();
+        HashSet<(int,int,int)> hash=new();
+        var result = new List<IList<int>>();
+        
         for(int i=0;i<nums.Length-2;i++)
         {
-            int sum=-nums[i];
-            int left=i+1;
-            int right=nums.Length-1;
-            while(left<right)
-            {
-                int tempsum=nums[left]+nums[right];
-                if(tempsum==sum)
-                {  List<int> temp=new();
-                  temp.Add(nums[i]);
-                  temp.Add(nums[left]);
-                  temp.Add(nums[right]);
-                  if(!set.Contains((nums[i],nums[left],nums[right])))
-                  {
-                    result.Add(temp);
-                    set.Add((nums[i],nums[left],nums[right]));
-                  }
-                
-                  right--;
-                  left++;
-                
-
-                }
-               else if(tempsum>sum)
-                right--;
-                else
-                left++;
-                
-            }
-
-        }
-        return result;
-
+	        int left=i+1;
+	        int right = nums.Length - 1;
         
+        while (left < right)
+        {
+            int sum = nums[left] + nums[right];
+            int target=-nums[i]; //toplamı 0 olan
+            if (sum == target)
+            {
+                if(!hash.Contains((nums[i],nums[left], nums[right])))
+                {
+                result.Add(new List<int> { nums[i],nums[left], nums[right] });
+                hash.Add((nums[i],nums[left], nums[right]));
+                }
+                
+                // Skip duplicates
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                //kod bitmesin devam etsin baska eslesmeler için 
+                left++;
+                right--;
+            }
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+        }
+        
+        return result;
     }
 }
