8024512c60b6bf17df03c37e7fac85dcba0003ce
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-04 16:18:19 +0300

    Time: 2 ms (88.30%) | Memory: 49.9 MB (36.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cs
+++ 36-valid-sudoku/valid-sudoku.cs
@@ -1,48 +1,33 @@
-
 public class Solution {
     public bool IsValidSudoku(char[][] board) {
-
-       
-        //row and column check
+        HashSet<char>[] rowhash=new HashSet<char>[9];
+        HashSet<char>[] columnhash=new HashSet<char>[9];
+        HashSet<char>[] sectionhash=new HashSet<char>[9];
         for(int i=0;i<9;i++)
         {
-            HashSet<char> checkrow=new();
-            HashSet<char> checkcolumn=new();
-            for(int j=0;j<9;j++)
-            {
-              if (board[i][j] != '.') {
-            if (checkrow.Contains(board[i][j]))
-                return false;
-            checkrow.Add(board[i][j]);
+            rowhash[i]=new();
+            columnhash[i]=new();
+            sectionhash[i]=new();
         }
 
-        if (board[j][i] != '.') {
-            if (checkcolumn.Contains(board[j][i]))
-                return false;
-            checkcolumn.Add(board[j][i]);
-        }
-            }
-            
-        }
-         for(int row=0;row<9;row+=3)
+        for(int i=0;i<9;i++)
         {
-            for(int column=0;column<9;column+=3)
+            for(int j=0;j<9;j++)
             {
-            HashSet<char> check=new();
-            for(int j=row;j<row+3;j++)
-            {
-                for(int k=column;k<column+3;k++)
+                if(board[i][j]=='.')
                 {
-                         if(board[j][k]=='.')
-                       continue;
-                    if(check.Contains(board[j][k]))
-                    return false;
-                    check.Add(board[j][k]);
+                    continue;
                 }
+                char item=board[i][j];
+                int sectionindex=(i/3)*(3)+j/3;
+                if(rowhash[i].Contains(item)||columnhash[j].Contains(item)||sectionhash[sectionindex].Contains(item))
+                return false;
+                rowhash[i].Add(item);
+                columnhash[j].Add(item);
+                sectionhash[sectionindex].Add(item);
             }
-            }
-        } 
+        }
         return true;
-      
         
-    }}
+    }
+}
