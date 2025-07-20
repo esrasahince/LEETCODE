032670db356817e2af5b9d53e2b944a33bb7feeb
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-20 11:11:22 +0300

    Time: 35 ms (64.87%) | Memory: 65.9 MB (63.46%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,41 +1,41 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        
+        List<IList<int>> result=new List<IList<int>>();
+        if(nums.Length<3)
+        return result;
         Array.Sort(nums);
-        HashSet<(int,int,int)> hash=new();
-        var result = new List<IList<int>>();
-        
         for(int i=0;i<nums.Length-2;i++)
         {
-	        int left=i+1;
-	        int right = nums.Length - 1;
-        
-        while (left < right)
-        {
-            int sum = nums[left] + nums[right];
-            int target=-nums[i]; //toplamı 0 olan
-            if (sum == target)
+            if(i>0&&nums[i]==nums[i-1])
+            continue;
+            int left=i+1;
+            int right=nums.Length-1;
+            while(left<right)
             {
-                if(!hash.Contains((nums[i],nums[left], nums[right])))
+              
+               int sum=nums[i]+nums[left]+nums[right];
+               if(sum==0)
+               {
+                 result.Add(new List<int>(){nums[i],nums[left],nums[right]});
+                while(left+1<right&&nums[left]==nums[left+1])
                 {
-                result.Add(new List<int> { nums[i],nums[left], nums[right] });
-                hash.Add((nums[i],nums[left], nums[right]));
+                    left++;
                 }
-                
-                // Skip duplicates
-                while (left < right && nums[left] == nums[left + 1]) left++;
-                while (left < right && nums[right] == nums[right - 1]) right--;
-                //kod bitmesin devam etsin baska eslesmeler için 
-                left++;
-                right--;
+                while(right-1>left&&nums[right]==nums[right-1])
+                {
+                    right--;
+                }
+                 left++;
+                 right--;
+               }
+               else if(sum>0)
+               right--;
+               else
+               left++;
+
             }
-            else if (sum < target)
-                left++;
-            else
-                right--;
         }
-        }
-        
         return result;
+        
     }
 }
