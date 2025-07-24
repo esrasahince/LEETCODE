3cf1de9e02e0902e5ecb7347c089b318b5f16c90
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-24 16:29:58 +0300

    Time: 0 ms (100.00%) | Memory: 50 MB (25.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,49 +1,34 @@
 public class Solution {
-    public int[] SearchRange(int[] nums, int target) {
-        if(nums==null||nums.Length<1)
+    public int[] SearchRange(int[] nums, int target)
+    {
+        if(nums==null||nums.Length==0)
         return new int[2]{-1,-1};
-        
-        int first=FirstPosition(nums,target);
-        int last=LastPosition(nums,target);
-        return new int[2]{first,last};
-        
-    }
-    public int FirstPosition(int[] nums,int target)
-    {
-        int left=0;
-        int right=nums.Length-1;
-        while(left<right)
-        {
-            int middle=left+(right-left)/2;
-           
-            if(nums[middle]<target)
-            left=middle+1;
-            else
-            right=middle;
-        }
-        //left==right durumunda döngü bitti
-        return nums[left]==target?left:-1;
 
-    }
-     public int LastPosition(int[] nums,int target)
-    {
-        int left=0;
-        int right=nums.Length-1;
-        int lastoccurence=-1;
-        while(left<=right)
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left < right)
         {
-            int middle=left+(right-left)/2;
-          
-            if(nums[middle]==target)
+            int middle = left + (right - left) / 2;
+            if (target > nums[middle])
             {
-                lastoccurence=middle;
-                left=middle+1;
+                left = middle + 1;
             }
-            else if(nums[middle]<target)
-            left=middle+1;
             else
-            right=middle-1;
+            {
+                right = middle;
+            }
+
         }
+        //it ends when left==middle;
+        if(nums[left]!=target)
+        return new int[2]{-1,-1};
+        while (right<nums.Length)
+        {
+            if(nums[right] != target)
+            break;
+            right++;
+        }
+        return new int[2] { left, right-1};
         
-        return lastoccurence;
-}}
+    }
+}
