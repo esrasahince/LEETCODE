162e68694f795a34bd47fe869fe43e227aa5043d
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-24 19:11:36 +0300

    Time: 0 ms (100.00%) | Memory: 42.8 MB (18.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,36 +1,37 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-
+        //target direk soruluyor  varlığından eminiz
+        //template 1
         int left=0;
         int right=nums.Length-1;
         while(left<=right)
         {
+
             int middle=left+(right-left)/2;
             if(nums[middle]==target)
             return middle;
-            //hangi taraf sıralı
-            if(nums[middle]<nums[right])
-            { //sağ taraf sıralı
-               if(nums[middle]<target&&target<=nums[right])
-               { 
-                left=middle+1;
-               }
-               else
-               {
-                right=middle-1;
-               }
+            if(nums[middle]<nums[right]) // sağ taraf sıralı
+            {
+                if(target>nums[middle]&&target<=nums[right])
+                {
+                    left=middle+1;
+                }
+                else
+                {
+                    right=middle-1;
+                }
 
             }
-            else
-            { //sol taraf sıralı
-              if(nums[middle]>target&&target>=nums[left])
-               { 
-                right=middle-1;
-               }
-               else
-               {
-                left=middle+1;
-               }
+            else //sol taraf sıralı
+            {
+                if(target<nums[middle]&&target>=nums[left])
+                {
+                    right=middle-1;
+                }
+                else
+                {
+                    left=middle+1;
+                }
 
             }
         }
