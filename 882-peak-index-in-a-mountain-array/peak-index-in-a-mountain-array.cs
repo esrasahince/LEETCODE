6bbd46bc13305daa369b8a3d74b2a1ae667f6e06
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-25 19:21:51 +0300

    Time: 0 ms (100.00%) | Memory: 68.5 MB (5.95%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cs
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cs
@@ -1,27 +1,23 @@
 public class Solution {
     public int PeakIndexInMountainArray(int[] arr) {
-        if(arr==null||arr.Length<1)
+        if(arr==null||arr.Length==0)
         return 0;
-
         int left=0;
         int right=arr.Length-1;
-        while(left+1<right)
+        while(left<right)
         {
             int middle=left+(right-left)/2;
-            //24,69,100,99,79,78,67,36,26,19]
-            //middle 79 , 67
-            if(arr[middle]>arr[middle+1]) // left 79, right 67
+          
+            if(arr[middle]<arr[middle+1])
             {
-                right=middle; 
+                left=middle+1;
             }
             else
             {
-                left=middle; //right 1
+                right=middle;
             }
         }
-        //left+1=right oldugunda dongu biter.
-        //iki tane adayımız var. left ve right
-        return arr[left]>arr[right]?left:right;
+        return left;
         
     }
 }
