f08652545e6bdbe5c244b22e856aad1ae21fbe09
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-27 20:50:16 +0300

    Time: 4 ms (99.02%) | Memory: 56.5 MB (68.74%) - LeetSync

--- 56-merge-intervals/merge-intervals.cs
+++ 56-merge-intervals/merge-intervals.cs
@@ -8,11 +8,10 @@ public class Solution {
 
         //sort according to start 
         Array.Sort(intervals,(a,b)=>a[0].CompareTo(b[0]));
-        result.Add(new int[2]{intervals[0][0],intervals[0][1]});
+        result.Add(intervals[0]);
         for(int i=1;i<intervals.Length;i++)
         {
             int x=result.Count-1;
-            int firststart=result[x][0];
             int firstend=result[x][1];
             int secondstart=intervals[i][0];
             int secondend=intervals[i][1];   
@@ -24,16 +23,10 @@ public class Solution {
              }
              else if( secondstart<=firstend)
              {
-                if(secondend<=firstend) //fully overlap
-                {
-                    continue;
-
-                }
-                else
-                { //halfoverlap, end points change
-                    result[x][1]=secondend;
+                
+                    result[x][1]=Math.Max(firstend,secondend);
                     
-                }
+                
              }
         }
 
