4b1bd458446f865e7683a74b5a1c9e9ce2dea330
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-29 08:29:49 +0300

    Time: 0 ms (100.00%) | Memory: 29.1 MB (47.33%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int GetSum(int a, int b) {
+        int sum=a^b;
+        int carry=(a&b)<<1;
+        while(carry!=0)
+        {
+            a=sum;
+            b=carry;
+            sum=a^b;
+            carry=(a&b)<<1;
+        }
+        return sum;
+        
+    }
+}
