39eed61faa57ed6583ed7bb127c00d8cb41ff9c4
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-30 08:29:35 +0300

    Time: 1 ms (58.55%) | Memory: 65.3 MB (51.16%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cs
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cs
@@ -1,21 +1,27 @@
 public class NumArray {
-    int[] prefix;
 
-    public NumArray(int[] nums) {
-        this.prefix=new int[nums.Length+1];
-      
-        for(int i=0;i<nums.Length;i++)
+    int[] prefixsum = null;
+    public NumArray(int[] nums)
+    {
+        this.prefixsum = new int[nums.Length];
+        if (nums.Length >= 1)
         {
-            prefix[i+1]=prefix[i]+nums[i];
+            prefixsum[0] = nums[0]; }
+        for (int i = 1; i < nums.Length; i++)
+        {
+            prefixsum[i] = nums[i] + prefixsum[i - 1];
         }
-        
+
     }
-    
-    public int SumRange(int left, int right) {
-     
-  
-        return prefix[right+1]-prefix[left];
-  
+
+    public int SumRange(int left, int right)
+    {
+        if (prefixsum == null)
+            return 0;
+        if(left==0)
+        return prefixsum[right];
+
+        return prefixsum[right] - prefixsum[left - 1];
         
     }
 }
