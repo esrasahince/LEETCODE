2d6367e4e08c8a1449517a55bb31e0f4415470c5
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-07-31 11:17:18 +0300

    Time: 1 ms (99.68%) | Memory: 60.9 MB (84.82%) - LeetSync

---
+++ 55-jump-game/jump-game.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int goal = nums.Length - 1;
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= goal)
+                goal = i;
+
+        }
+        if (goal == 0)
+            return true;
+        return false;
+
+       
+
+
+       
+     
+       
+    }
+
+}
