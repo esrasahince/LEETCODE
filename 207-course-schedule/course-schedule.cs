de300b6e5156a6f232acbb9d5e50747f5965b121
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-08-03 14:06:59 +0300

    Time: 5 ms (80.68%) | Memory: 50.6 MB (53.31%) - LeetSync

--- 207-course-schedule/course-schedule.cs
+++ 207-course-schedule/course-schedule.cs
@@ -1,45 +1,44 @@
 public class Solution {
-    public bool CanFinish(int n, int[][] prerequisites) {
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        Dictionary<int,List<int>> dict=new();
+        int[] incoming=new int[numCourses];
+        foreach(var item in prerequisites)
+        {
+            int from=item[1];
+            int to=item[0];
+            incoming[to]++;
+            if(!dict.ContainsKey(from))
+            dict[from]=new List<int>();
+            dict[from].Add(to);
 
-        Dictionary<int, List<int>> graph = new();
-        Dictionary<int, int> visited = new();
-
-        // initialize
-        for (int i = 0; i < n; i++) {
-            graph[i] = new List<int>();
-            visited[i] = 0;
         }
+        Queue<int> que=new();
+        for(int i=0;i<numCourses;i++)
+        {
+            if(incoming[i]==0)
+            que.Enqueue(i);
 
-        // build graph
-        foreach (var pre in prerequisites) {
-            int to = pre[0];
-            int from = pre[1];
-            graph[from].Add(to);
+
         }
+        int count=0;
+        while(que.Count>0)
+        {
+            int item=que.Dequeue();
+              count++;
+            if(!dict.ContainsKey(item))
+            continue;
+          
+            foreach(var ngb in dict[item])
+            {
+                incoming[ngb]--;
+                if(incoming[ngb]==0)
+                que.Enqueue(ngb);
 
-        // dfs check for cycle
-        for (int i = 0; i < n; i++) {
-            if (HasCycle(graph, visited, i))
-                return false;
+            }
         }
-
+        if(count==numCourses)
         return true;
-    }
-
-    private bool HasCycle(Dictionary<int, List<int>> graph, Dictionary<int, int> visited, int current) {
-        if (visited[current] == 1)
-            return true; // cycle detected
-        if (visited[current] == 2)
-            return false; // already visited
-
-        visited[current] = 1; // mark as visiting
-
-        foreach (var neighbor in graph[current]) {
-            if (HasCycle(graph, visited, neighbor))
-                return true;
-        }
-
-        visited[current] = 2; // mark as visited
         return false;
+        
     }
 }
