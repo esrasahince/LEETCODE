ea095ca0bdaa04df9986415d4f702c9e435c48a9
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-08-03 16:57:44 +0300

    Time: 244 ms (75.15%) | Memory: 60.9 MB (76.07%) - LeetSync

--- 744-network-delay-time/network-delay-time.cs
+++ 744-network-delay-time/network-delay-time.cs
@@ -1,63 +1,49 @@
 public class Solution {
     public int NetworkDelayTime(int[][] times, int n, int k) {
-  // 1. Adjacency list
-        Dictionary<int, List<(int, int)>> graph = new();
-        foreach (var item in times) {
-            int u = item[0], v = item[1], w = item[2];
-            if (!graph.ContainsKey(u)) graph[u] = new();
-            graph[u].Add((v, w));
-        }
-    // 2. Initialize distances
-        Dictionary<int, int> weight = new();
-        for (int i = 1; i <= n; i++) {
-            weight[i] = int.MaxValue;
-        }
-        weight[k] = 0;
-
-     // 3. Priority Queue (min-heap)
-        PriorityQueue<int, int> que = new();
-        que.Enqueue(k, 0);
-
-    // 4. Dijkstra
-        HashSet<int> visited = new();
-
-    while(que.Count>0)
-    {
-        que.TryDequeue(out int currentnode,out int currentweight);
-        if(visited.Contains(currentnode))
-        continue;
-        if(!graph.ContainsKey(currentnode)) //hic kenarı olmadıgı için grapha eklenmemiş olabilri
-        continue;
-        visited.Add(currentnode);
-        
-        foreach(var (child,childweight) in graph[currentnode])
+        int[] distances=new int[n+1];
+       int result=0;
+        Dictionary<int,List<(int,int)>> graph=new();
+        for(int i=0;i<=n;i++)
         {
-            if(!visited.Contains(child))
+            distances[i]=int.MaxValue;
+            graph[i]=new List<(int,int)>();
+        }
+        distances[k]=0;
+        foreach(var time in times)
+        {
+            int from=time[0];
+            int to=time[1];
+            int weight=time[2];
+            graph[from].Add((to,weight));
+        }
+        PriorityQueue<int,int> que=new();
+        que.Enqueue(k,0);
+        while(que.Count>0)
+        {
+            que.TryDequeue( out int current,out int currentdist);
+            if(distances[current]<currentdist)
+            continue;
+            foreach((int ngb,int weight) in graph[current])
             {
-                int newweight=currentweight+childweight;
-                if(newweight<weight[child]) //Adan Bye 8, Adan Cye 1, C den B ye 2 olabilir. 
+                if(weight+currentdist<distances[ngb])
                 {
-                 que.Enqueue(child,newweight);
-                weight[child]=newweight;
+                    distances[ngb]=weight+currentdist;
+                      que.Enqueue(ngb,distances[ngb]);
+                    
                 }
-            
-                
+              
+
             }
+
         }
-    }
+   for (int i = 1; i <= n; i++) {
+    if (distances[i] == int.MaxValue)
+        return -1;
+    result = Math.Max(result, distances[i]);
+}
+return result;
 
-        // 5. Weight dictionaryde  artık en azz weightler var.
-        int maxpath=0;//tüm nodelara sinyal ulasması icin en büyüğü almam laızm
-        foreach(int value in weight.Values)
-        {
-            if(value==int.MaxValue)// bu node a k dan giden hiç bir path olmamıs demektir
-            return -1;
-            maxpath=Math.Max(maxpath,value);
-        }
-        return maxpath;
-
-    
-
-    
+     
+        
     }
 }
