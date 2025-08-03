e38f70bebb24c1c08f8819f2f280aaeeaadbbc3d
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-08-03 21:28:21 +0300

    Time: 30 ms (12.68%) | Memory: 49.4 MB (90.22%) - LeetSync

--- 47-permutations-ii/permutations-ii.cs
+++ 47-permutations-ii/permutations-ii.cs
@@ -1,39 +1,33 @@
 public class Solution {
     public IList<IList<int>> PermuteUnique(int[] nums) {
-        List<IList<int>> result=new();
-        List<int> temp=new();
-        Array.Sort(nums);
-        bool[] visited=new bool[nums.Length];
-        Backtracking(nums,visited,temp,result);
+    Array.Sort(nums);
+    List<IList<int>> result=new();
+    List<int> temp=new();
+         HashSet<int> check=new();
+        DFS(nums,0,result,temp,check);
         return result;
+        
     }
-    public void Backtracking(int[] nums,bool[] visited,List<int> temp,List<IList<int>> result)
+
+    public void DFS(int[] nums,int index, List<IList<int>> result,List<int> temp,HashSet<int> check)
     {
         if(temp.Count==nums.Length)
         {
             result.Add(new List<int>(temp));
             return;
         }
-        for(int i=0;i<nums.Length;i++)
-        {
-        if(!visited[i])
-        {
-            if(i>0&&nums[i]==nums[i-1]&&!visited[i-1])
-            {
-                continue;
-            }
-            visited[i]=true;
+         for(int i=0;i<nums.Length;i++)
+        { 
+          if(check.Contains(i))
+          continue;
+          if(i!=0&&nums[i]==nums[i-1]&&check.Contains(i-1))
+          continue;
             temp.Add(nums[i]);
-            Backtracking(nums,visited,temp,result);
-            visited[i]=false;
+            check.Add(i);
+            DFS(nums,i,result,temp,check);
             temp.RemoveAt(temp.Count-1);
-                
-            
+            check.Remove(i);
         }
-        }
-    }
-
-
         
-    
+    }
 }
