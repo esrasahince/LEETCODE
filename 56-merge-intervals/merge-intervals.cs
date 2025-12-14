3e39896629355025f462712f71b420a68c6b8b3c
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-12-14 10:18:56 +0300

    Time: 5 ms (57.53%) | Memory: 56.7 MB (57.93%) - LeetSync

--- 56-merge-intervals/merge-intervals.cs
+++ 56-merge-intervals/merge-intervals.cs
@@ -1,37 +1,35 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        if(intervals==null)
-        return null;
-        if(intervals.Length<=1)
+
+        //x1,y1  x2,y2
+
+        //sort result x1<=x2
+        //if(x1==x2)=> x1, Max(y1,y2)
+        //if(y1<=x2)=> x1, y2
+         //else(y1>x2)=> x1, Max(y1,y2)
+
+
+        if(intervals.Length==0)
         return intervals;
+
         List<int[]> result=new();
 
-        //sort according to start 
         Array.Sort(intervals,(a,b)=>a[0].CompareTo(b[0]));
-        result.Add(intervals[0]);
+
+        result.Add(new int[2]{intervals[0][0],intervals[0][1]});
+
         for(int i=1;i<intervals.Length;i++)
         {
-            int x=result.Count-1;
-            int firstend=result[x][1];
-            int secondstart=intervals[i][0];
-            int secondend=intervals[i][1];   
+            int len=result.Count-1;
+            if(intervals[i][0]==result[len][0]|| result[len][1]>=intervals[i][0])
+            {
+                result[len][1]=Math.Max(result[len][1],intervals[i][1]);
+            }
+            else if(result[len][1]<intervals[i][0])
+             result.Add(new int[2]{intervals[i][0],intervals[i][1]});
              
-             //discrete
-             if(firstend<secondstart)
-             {
-                result.Add(new int[2]{secondstart,secondend});
-             }
-             else if( secondstart<=firstend)
-             {
-                
-                    result[x][1]=Math.Max(firstend,secondend);
-                    
-                
-             }
         }
-
         return result.ToArray();
-
         
     }
 }
