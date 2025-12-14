9950599f9bd54ced75baf95891b4a2cc00d96ff7
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-12-14 11:13:55 +0300

    Time: 99 ms (75.28%) | Memory: 47.5 MB (72.84%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cs
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -11,15 +11,16 @@
  */
 public class Solution {
     public bool HasCycle(ListNode head) {
+
         if(head==null)
         return false;
-
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null&&fast.next!=null)
         {
-            fast=fast.next.next;
             slow=slow.next;
+        
+            fast=fast.next.next;
             if(fast==slow)
             return true;
         }
