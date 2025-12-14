42ba1404b6cf666defe033388d684ce6b58c9c5b
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-12-14 09:35:32 +0300

    Time: 6 ms (55.24%) | Memory: 44 MB (57.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -1,31 +1,48 @@
 public class Solution {
-    public int LengthOfLongestSubstring(string s) {
-        if(String.IsNullOrEmpty(s))
-        return 0;
-        if(s.Length==1)
-        return 1;
-        HashSet<char> hash=new();
-        int left=0;
-        int result=0;
-        for(int right=0;right<s.Length;right++)
+  public int LengthOfLongestSubstring(string s) {
+    if(String.IsNullOrEmpty(s))
+    return 0;
+    if(String.IsNullOrWhiteSpace(s))
+    return 1;
+
+        HashSet<char> check=new();
+        int result=1;
+        int leftptr=0;
+        //"abcabcbb"
+        //left: 0 i:0  check=a
+        //left: 0 i:1  check=a-b
+        //left: 0 i:2  check=a-b-c
+        //left: 0 i:3  check=a-b-c  result Math.MAx(0, 3-0)=3
+           // s[leftptr]=a==a  leftptr=1  check b-c
+        //left: 1 i:4  check=b-c  result Math.MAx(3, 4-1)=3
+        
+
+          
+
+
+        for(int i=0;i<s.Length;i++)
         {
-            char item=s[right];
-            if(hash.Contains(item))
+            char item=s[i];
+            if(check.Contains(item))
             {
-               while(hash.Contains(item))
-               {
-                  hash.Remove(s[left]);
-                  left++;
-               }
-               hash.Add(item);
+                result=Math.Max(result,i-leftptr);
+                while(s[leftptr]!=item)
+                {
+                    check.Remove(s[leftptr]);
+                    leftptr++;
+                }
+                check.Remove(s[leftptr]);
+                leftptr++;
+
+
+
             }
-            else
-            {
-                hash.Add(item);
-            }
-            result=Math.Max(result,right-left+1);
+            check.Add(item);
+
         }
-        return result;
+
+        return Math.Max(result,check.Count);
         
     }
+
 }
