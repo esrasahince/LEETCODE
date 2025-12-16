178afe1188cbafa45ca993a8d369b5d225c0e554
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2025-12-16 17:38:25 +0300

    Time: 167 ms (7.06%) | Memory: 52.1 MB (96.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.cs
+++ 200-number-of-islands/number-of-islands.cs
@@ -1,38 +1,40 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
+
+        HashSet<(int,int)> visited=new();
         int result=0;
+
         for(int i=0;i<grid.Length;i++)
         {
             for(int j=0;j<grid[0].Length;j++)
             {
-                   if(grid[i][j]=='1')
-                   {
-                    DFS(grid,i,j);
-                    result++;
-                   }
-
+                if(visited.Contains((i,j))||grid[i][j]=='0')
+                continue;
+                DFS(grid,i,j,visited);
+                result++;
             }
-         
         }
         return result;
-
+        
         
     }
-    public void DFS(char[][] grid,int row,int column)
+
+    public void DFS(char[][] grid,int row,int column,HashSet<(int,int)> visited)
     {
-        int rsize=grid.Length;
-        int csize=grid[0].Length;
+        if(row>=grid.Length||column>=grid[0].Length||row<0||column<0)
+        return;
+        if(visited.Contains((row,column)))
+        return;
+        if(grid[row][column]=='0')
+        return;
 
-        if(row>=rsize||row<0||column>=csize||column<0)
-        return;
-        if(grid[row][column]=='0'||grid[row][column]=='2')
-        return;
-        grid[row][column]='2';
         
+        visited.Add((row,column));
 
-        DFS(grid,row+1,column);
-        DFS(grid,row-1,column);
-        DFS(grid,row,column+1);
-        DFS(grid,row,column-1);
+        DFS(grid,row+1,column,visited);
+        DFS(grid,row-1,column,visited);
+        DFS(grid,row,column+1,visited);
+        DFS(grid,row,column-1,visited);
     }
+
 }
