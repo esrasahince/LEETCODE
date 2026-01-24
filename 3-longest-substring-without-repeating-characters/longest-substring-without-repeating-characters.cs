3f2e6793b09fc2b22b5e7c5e3756286b8aa98978
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2026-01-24 22:00:31 +0300

    Time: 7 ms (41.60%) | Memory: 43.7 MB (84.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -1,48 +1,32 @@
 public class Solution {
-  public int LengthOfLongestSubstring(string s) {
-    if(String.IsNullOrEmpty(s))
-    return 0;
-    if(String.IsNullOrWhiteSpace(s))
-    return 1;
-
-        HashSet<char> check=new();
-        int result=1;
-        int leftptr=0;
-        //"abcabcbb"
-        //left: 0 i:0  check=a
-        //left: 0 i:1  check=a-b
-        //left: 0 i:2  check=a-b-c
-        //left: 0 i:3  check=a-b-c  result Math.MAx(0, 3-0)=3
-           // s[leftptr]=a==a  leftptr=1  check b-c
-        //left: 1 i:4  check=b-c  result Math.MAx(3, 4-1)=3
-        
-
-          
-
-
-        for(int i=0;i<s.Length;i++)
+    public int LengthOfLongestSubstring(string s) {
+        int left=0;
+        int right=0;
+        HashSet<char> set=new();
+        int result=0;
+        while(right<s.Length)
         {
-            char item=s[i];
-            if(check.Contains(item))
+            if(!set.Contains(s[right]))
             {
-                result=Math.Max(result,i-leftptr);
-                while(s[leftptr]!=item)
-                {
-                    check.Remove(s[leftptr]);
-                    leftptr++;
-                }
-                check.Remove(s[leftptr]);
-                leftptr++;
-
-
+                set.Add(s[right]);
+                right++;
 
             }
-            check.Add(item);
-
+            else
+            {
+                result=Math.Max(result,right-left);
+                while(s[left]!=s[right]&&left<=right)
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+                 set.Remove(s[left]);
+                left++;
+            }
         }
+        result=Math.Max(result,right-left);
 
-        return Math.Max(result,check.Count);
+        return result;
         
     }
-
 }
