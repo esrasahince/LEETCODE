c760b079ea076f3ebf255e20534b60ecb7c80dc6
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2026-01-24 13:04:22 +0300

    Time: 2 ms (52.23%) | Memory: 68.1 MB (30.05%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -1,25 +1,33 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        
-        int[] prefix=new int[nums.Length];
-        int[] suffix=new int[nums.Length];
-        prefix[0]=nums[0];
-       
+        int[] pre=new int[nums.Length];
+        int[] post=new int[nums.Length];
+        pre[0]=nums[0];
+        post[post.Length-1]=nums[nums.Length-1];
         for(int i=1;i<nums.Length;i++)
         {
-            prefix[i]=prefix[i-1]*nums[i];
-        
+            
+            pre[i]=nums[i]*pre[i-1];
+
         }
-        int post=1;
-         for(int i=nums.Length-1;i>0;i--)
+         for(int i=nums.Length-2;i>=0;i--)
         {
-            prefix[i]=post*prefix[i-1];
-            post=post*nums[i];
-
-
+            post[i]=nums[i]*post[i+1];
         }
-        prefix[0]=post;
-        return prefix;
+         for(int i=0;i<nums.Length;i++)
+        {
+            if(i==0)
+            nums[i]=post[1];
+            else if(i==nums.Length-1)
+            nums[i]=pre[i-1];
+            else
+
+            if(i>0)
+            {
+                nums[i]=pre[i-1]*post[i+1];
+            }
+        }
+
+        return nums;
     }
- 
 }
