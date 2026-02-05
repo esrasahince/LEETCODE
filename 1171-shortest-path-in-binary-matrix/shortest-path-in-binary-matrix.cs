53fbe576e5c495896c3bb13f960eeb00b8aed43f
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2026-02-05 22:39:08 +0300

    Time: 14 ms (40.10%) | Memory: 53.9 MB (76.24%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cs
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cs
@@ -1,43 +1,37 @@
 public class Solution {
     public int ShortestPathBinaryMatrix(int[][] grid) {
-         int n=grid.Length-1;
-        if(grid[0][0]==1||grid[n][n]==1)
+        if(grid[0][0]!=0||grid[grid.Length-1][grid[0].Length-1]!=0)
         return -1;
+        return BFS(grid);
+
+        
+    }
+    public int BFS(int[][] grid)    
+    {
+
         Queue<(int,int,int)> que=new();
         que.Enqueue((0,0,1));
-         grid[0][0]=2;
-        int result=int.MaxValue;
-        int[][] direction=new int[8][]{
-            [-1,-1],
-            [-1,0],
-            [-1,1],
-            [1,0],
-            [1,1],
-            [1,-1],
-            [0,-1],
-            [0,1]};
-       
+        grid[0][0]=2;
+        List<(int,int)> directions=new(){(0,1),(1,0),(-1,0),(0,-1),(1,1),(-1,-1),(-1,1),(1,-1)};
         while(que.Count>0)
         {
-            var(row,column,dist)=que.Dequeue();
-            if(row==n&&column==n)
+            var (row,column,distance)=que.Dequeue();
+            if(row==grid.Length-1&&column==grid[0].Length-1)
+            return distance;
+            
+            foreach(var item in directions)
             {
-                result=Math.Min(result,dist);
-            }
-            for(int i=0;i<direction.Length;i++)
-            {
-                int r=row+direction[i][0];
-                int c=column+direction[i][1];
-                if(r>=0&&r<=n&&c>=0&&c<=n&&grid[r][c]==0)
-                {
-                    que.Enqueue((r,c,dist+1));
-                    grid[r][c]=2;
-                }
+                int newrow=row+item.Item1;
+                int newcolumn=column+item.Item2;
+                if(newrow<0||newrow>=grid.Length||newcolumn<0||newcolumn>=grid[0].Length||grid[newrow][newcolumn]==1||grid[newrow][newcolumn]==2)
+                continue;
+                que.Enqueue((newrow,newcolumn,distance+1));
+                grid[newrow][newcolumn]=2;
+
             }
         }
-          
-        
-        return result==int.MaxValue?-1:result;
-        
+        return -1;
+
+
     }
 }
