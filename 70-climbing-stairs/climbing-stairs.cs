109534e8a2dd18ca29959912c6c458d35b3c6dbe
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2026-02-06 16:23:44 +0300

    Time: 0 ms (100.00%) | Memory: 29.5 MB (14.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cs
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -1,21 +1,24 @@
 public class Solution {
-    public int ClimbStairs(int n) {
-        Dictionary<int,int> memory=new();
+    public int ClimbStairs(int n)
+            {
+                Dictionary<int,int> dict=new();
+            return Recursive(n, n,dict);
+            }
 
-        return DP(n,0,memory);
-        
-    }
+            public int Recursive(int n,int current, Dictionary<int,int> dict)
+        {
+            if (current < 0)
+                return 0;
+            if (current<=1)
+               return 1;
+            if(dict.ContainsKey(current))
+            return dict[current];
+            
+            int onestep = Recursive(n, current - 1,dict);
+            int twostep= Recursive(n, current - 2,dict);
 
-    public int DP(int n,int current,Dictionary<int,int> memory)
-    {
-        if(current>n)
-        return 0;
-        if(current==n)
-        return 1;
-        if(memory.ContainsKey(current))
-        return memory[current];
-        memory[current]=DP(n,current+1,memory)+DP(n,current+2,memory);
-        return memory[current];
+         int result=onestep + twostep;
+         dict[current]=result;
+         return result;
 
-    }
-}
+        }}
