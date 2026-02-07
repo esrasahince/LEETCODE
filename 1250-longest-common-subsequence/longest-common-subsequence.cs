5188fe63068a02725ae4f7de3927ba88c2a2611e
Esra ŞAHİN <112387335+esrasahince@users.noreply.github.com>
2026-02-07 16:54:34 +0300

    Time: 14 ms (69.86%) | Memory: 43.9 MB (61.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cs
+++ 1250-longest-common-subsequence/longest-common-subsequence.cs
@@ -1,29 +1,24 @@
 public class Solution {
     public int LongestCommonSubsequence(string text1, string text2) {
-        if(text1==null||text2==null)
-        return 0;
-        Dictionary<(int,int),int> memo=new();
-        return DP(text1,text2,text1.Length-1, text2.Length-1,memo);
-        
-    }
-    public int DP(string s1, string s2, int index1,int index2,Dictionary<(int,int),int> memo)
-    {
-        if(index1<0||index2<0)
-        return 0;
-        if(memo.ContainsKey((index1,index2)))
-        return memo[(index1,index2)];
-        int result=0;
-        if(s1[index1]==s2[index2])
-        {
-            result=1+DP(s1,s2,index1-1,index2-1,memo);
-        }
-        else
-        {
-            result+=Math.Max(DP(s1,s2,index1-1,index2,memo),DP(s1,s2,index1,index2-1,memo));
 
+        int n=text1.Length;
+        int m=text2.Length;
+        int[,] dp=new int[n+1,m+1];
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i,j]=1+dp[i-1,j-1];
+                }
+                else
+                {
+                    dp[i,j]=Math.Max(dp[i,j-1],dp[i-1,j]);
+                }
+            }
         }
-        memo[(index1,index2)]=result;
-        return result;
+        return dp[n,m];
         
     }
 }
